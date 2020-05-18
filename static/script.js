5d54e3d3cc4ef00cc07d83f975dbcde75fb6de66
Stanton Parham <stparham@ncsu.edu>
2020-05-18 12:57:47 -0400

    Add Google Maps to main page

--- static/script.js
+++ static/script.js
@@ -1,7 +1,21 @@
 'use strict';
 
-window.addEventListener('load', function () {
+/** The HTML element that holds the map. */
+const MAP_EL_ID = "map";
+/** Options for the map. */
+const MAP_OPTIONS = {
+  center: {
+    lat: 35.1013734,
+    lng: -76.5595641
+  },
+  zoom: 8
+};
 
-  console.log("Hello World!");
+let map;
 
-});
+/**
+ * Initializes the main map.
+ */
+function initMap() {
+  map = new google.maps.Map(document.getElementById(MAP_EL_ID), MAP_OPTIONS);
+}
