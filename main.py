5b6d9da6720908207d72b439a11aae418e5edf50
Stanton Parham <stparham@ncsu.edu>
2020-05-27 16:02:30 -0400

    Made a createApp function

--- main.py
+++ main.py
@@ -8,21 +8,30 @@ from routes.areaData import areaData
 
 from config import Config, DevConfig
 
-app = Flask(__name__)
-
-app.register_blueprint(pages)
-app.register_blueprint(areaData)
-
-def initialize(configObj):
+def createApp(configObj):
+  """
+  Returns a Flask app configured with the given configuration object.
+  """
+  app = Flask(__name__)
   app.config.from_object(configObj)
+
+  # register blueprints
+  app.register_blueprint(pages)
+  app.register_blueprint(areaData)
+
+  # initialize database connection
   db.init_app(app)
+
+  return app
+
+app = None
 
 # if running the file directly
 if __name__ == '__main__':
   # setup for running locally (development configuration)
-  initialize(DevConfig())
+  app = createApp(DevConfig())
   # run the app locally
   app.run(host=DevConfig.HOST, port=DevConfig.PORT, debug=True)
 else: # else the app is being run from a WSGI application such as gunicorn
   # setup for production configuration
-  initialize(Config())
+  app = createApp(Config())
