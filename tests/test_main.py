8612ab32d37678b9e694f9851abde428d5019c39
Stanton Parham <stparham@ncsu.edu>
2020-05-27 16:37:34 -0400

    Update tests to use createApp; add more tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,16 +1,31 @@
 import pytest
 
-import main
+from main import createApp
 
 from config import TestConfig
 
 @pytest.fixture
 def client():
-  main.app.config.from_object(TestConfig())
+  app = createApp(TestConfig())
 
-  with main.app.test_client() as client:
+  with app.test_client() as client:
     yield client
 
 def test_index(client):
   res = client.get('/')
   assert res.status_code == 200
+
+def test_about(client):
+  res = client.get('/about')
+  assert res.status_code == 200
+
+def test_notifications(client):
+  res = client.get('/notifications')
+  assert res.status_code == 200
+
+def test_areaData(client):
+  res = client.get('/areaData')
+  assert res.status_code == 200
+  json = res.get_json()
+  assert len(json) == 73
+  assert json['A01']['prob1Day'] == 16
