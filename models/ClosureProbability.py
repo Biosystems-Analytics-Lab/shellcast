30c2f5049e04c6b5249a7da352b28603237979b7
Stanton Parham <stparham@ncsu.edu>
2020-06-01 09:40:45 -0400

    Add SGAInfo model and create relationship between it and ClosureProbability model

--- models/ClosureProbability.py
+++ models/ClosureProbability.py
@@ -3,7 +3,7 @@ from models import db
 class ClosureProbability(db.Model):
   __tablename__ = 'sga_closure_probabilities'
   id = db.Column(db.Integer, primary_key=True)
-  sga_id = db.Column(db.Integer)
+  sga_id = db.Column(db.Integer, db.ForeignKey('sga_info.id'))
   day = db.Column(db.Integer)
   rain_forecast = db.Column(db.Float)
   prob_percent = db.Column(db.Float)
@@ -11,5 +11,12 @@ class ClosureProbability(db.Model):
   created = db.Column(db.DateTime)
   updated = db.Column(db.DateTime)
 
+  sgaInfo = db.relationship('SGAInfo', back_populates='closureProbabilities')
+
+  def to_dict(self):
+    return {
+      'prob1Day': self.prob_percent,
+    }
+
   def __repr__(self):
-    return '<SGAClosureProbability: {}>'.format(self.id)
+    return '<ClosureProbability(id={},sga_id={},perc={})>'.format(self.id, self.sga_id, self.prob_percent)
