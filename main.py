16ac474cc1ac040011039fd84b8a21186f895e67
Stanton Parham <stparham@ncsu.edu>
2020-06-02 15:00:33 -0400

    Integrate Google Cloud logging with Python logging module

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import datetime
+import os
+import logging
+
 from flask import Flask, render_template, jsonify
 
 from models import db
@@ -7,6 +9,17 @@ from routes.pages import pages
 from routes.areaData import areaData
 
 from config import Config, DevConfig
+
+# check if running on Google App Engine
+# (just checking for one of the environment variables found here: https://cloud.google.com/appengine/docs/standard/python3/runtime#environment_variables)
+if ('GAE_APPLICATION' in os.environ):
+  # integrate Python logging module with Google Cloud Logging (captures INFO level and higher by default)
+  import google.cloud.logging
+  gcloudLoggingClient = google.cloud.logging.Client()
+  gcloudLoggingClient.get_default_handler()
+  gcloudLoggingClient.setup_logging()
+
+  logging.info('Running on Google App Engine')
 
 def createApp(configObj):
   """
@@ -28,10 +41,13 @@ app = None
 
 # if running the file directly
 if __name__ == '__main__':
+  logging.info('Starting app with development configuration')
   # setup for running locally (development configuration)
   app = createApp(DevConfig())
   # run the app locally
   app.run(host=DevConfig.HOST, port=DevConfig.PORT, debug=True)
 else: # else the app is being run from a WSGI application such as gunicorn
+  logging.info('Starting app with production configuration')
+
   # setup for production configuration
   app = createApp(Config())
