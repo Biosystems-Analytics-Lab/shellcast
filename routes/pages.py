f5e8028f8e9a7c2ff4f34c568a2d7e333616a921
Stanton Parham <stparham@ncsu.edu>
2020-06-02 15:01:21 -0400

    Add some test logs to the test route

--- routes/pages.py
+++ routes/pages.py
@@ -1,16 +1,12 @@
 from flask import Blueprint, render_template, current_app
 from models.ClosureProbability import ClosureProbability
 
+import logging
+
 pages = Blueprint('', __name__)
 @pages.route('/')
 def indexPage():
   return render_template('index.html.jinja', mapsAPIKey=current_app.config['MAPS_API_KEY'])
-
-
-@pages.route('/test')
-def test():
-  print(ClosureProbability.query.all())
-  return 'blah'
 
 @pages.route('/about')
 def aboutPage():
@@ -19,4 +15,15 @@ def aboutPage():
 @pages.route('/notifications')
 def notificationsPage():
   return render_template('notifications.html.jinja')
+
+@pages.route('/super-special-extra-secret-test-route')
+def test():
+  print('Test print: blah blah')
+  logging.debug('Test logging.debug: superfluous stuff')
+  logging.info('Test logging.info: normal stuff')
+  logging.warning('Test logging.warning: potentially bad stuff')
+  logging.error('Test logging.error: bad stuff')
+  logging.exception('Test logging.exception: worse stuff')
+  logging.critical('Test logging.critical: absolutely terrible stuff')
+  return 'This is just a testing route.  How\'d you find this?'
 
