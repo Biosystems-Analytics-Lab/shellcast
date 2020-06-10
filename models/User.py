751cf581f360008f3a6cd99b5897941c195f29cc
Stanton Parham <stparham@ncsu.edu>
2020-06-10 10:13:15 -0400

    Add User model and tests for models

---
+++ models/User.py
@@ -0,0 +1,23 @@
+from models import db
+# from models.Subscription import Subscription
+# from models.Notification import Notification
+
+class User(db.Model):
+  __tablename__ = 'users'
+
+  id = db.Column(db.Integer, primary_key=True)
+  firebase_uid = db.Column(db.String(28))
+  first_name = db.Column(db.String(50))
+  last_name = db.Column(db.String(50))
+  phone_number = db.Column(db.String(11))
+  email = db.Column(db.String(50))
+  sms_pref = db.Column(db.Boolean())
+  email_pref = db.Column(db.Boolean())
+  created = db.Column(db.DateTime, server_default=db.func.now())
+  updated = db.Column(db.DateTime, server_default=db.func.now(), onupdate=db.func.now())
+
+  # subscriptions = db.relationship('Subscription', order_by=Subscription.created, back_populates='user')
+  # notifications = db.relationship('Notification', order_by=Notification.created, back_populates='user')
+
+  def __repr__(self):
+    return '<User: {}>'.format(self.email)
