c3df1bf37ba6a635612e3eb4a497e5905ae4fbc7
Stanton Parham <stparham@ncsu.edu>
2020-06-11 11:46:04 -0400

    Add test fixture that monkey patches Firebase token verification
    This ensures that testing on all routes that use Firebase token verification can occur.  Without this, a lot of routes would have had to go untested which wouldn't have been good.

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,6 +5,9 @@ from models import db as _db
 
 from config import TestConfig
 
+from firebase_admin import auth
+from firebase_admin.auth import ExpiredIdTokenError, InvalidIdTokenError
+
 # will be run once at the beginning of the testing session
 @pytest.fixture(scope='session')
 def app():
@@ -46,3 +49,31 @@ def dbSession(db):
   transaction.rollback()
   connection.close()
   session.remove()
+
+@pytest.fixture(scope='function', autouse=True)
+def addMockFbUser():
+  """
+  Monkey patches Firebase token verification and yields a function that can
+  be used to create mock Firebase users and their ID tokens.
+  """
+  mockFirebaseUsers = {}
+
+  def mockTokenVerification(token):
+    if (token not in mockFirebaseUsers):
+      raise InvalidIdTokenError('{} is not a valid token.'.format(token))
+    user = mockFirebaseUsers[token]
+    if (user['tokenExpired']):
+      raise ExpiredIdTokenError('{} is expired.'.format(token), None)
+    return user['userInfo']
+
+  # replace the verify_id_token definition
+  realTokenVerification = auth.verify_id_token
+  auth.verify_id_token = mockTokenVerification
+
+  def addUser(userInfo, token, tokenIsExpired=False):
+    mockFirebaseUsers[token] = dict(tokenExpired=tokenIsExpired, userInfo=userInfo)
+
+  yield addUser
+
+  # restore true token verification
+  auth.verify_id_token = realTokenVerification
