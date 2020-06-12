3ef18e87d1cffb4f0f8f9396542a78a16c79e382
Stanton Parham <stparham@ncsu.edu>
2020-06-12 11:24:46 -0400

    Update SGA info model and add tests

---
+++ models/SGAMinMaxProbability.py
@@ -0,0 +1,29 @@
+from models import db
+
+class SGAMinMaxProbability(db.Model):
+  __tablename__ = 'sga_min_max'
+
+  id = db.Column(db.Integer, primary_key=True)
+  grow_area_name = db.Column(db.String(3))
+  min_1d_prob = db.Column(db.Integer)
+  max_1d_prob = db.Column(db.Integer)
+  min_2d_prob = db.Column(db.Integer)
+  max_2d_prob = db.Column(db.Integer)
+  min_3d_prob = db.Column(db.Integer)
+  max_3d_prob = db.Column(db.Integer)
+  created = db.Column(db.DateTime)
+  updated = db.Column(db.DateTime)
+
+  def asDict(self):
+    return {
+      'grow_area_name': self.grow_area_name,
+      'min_1d_prob': self.min_1d_prob,
+      'max_1d_prob': self.max_1d_prob,
+      'min_2d_prob': self.min_2d_prob,
+      'max_2d_prob': self.max_2d_prob,
+      'min_3d_prob': self.min_3d_prob,
+      'max_3d_prob': self.max_3d_prob,
+    }
+
+  def __repr__(self):
+    return '<SGAMinMax: {}>'.format(self.grow_area_name)
