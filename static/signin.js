19a5dd8ff4474046bdb4a8c8dd3310dc74db33e1
Stanton Parham <stparham@ncsu.edu>
2020-06-15 16:47:14 -0400

    Add dedicated signin page

---
+++ static/signin.js
@@ -0,0 +1,24 @@
+const FIREBASE_UI_CONFIG = {
+  signInSuccessUrl: '/',
+  callbacks: {
+    // signInSuccessWithAuthResult: (authResult, redirectUrl) => {},
+    signInFailure: (error) => {
+      console.log('Failure during sign in');
+      console.log(error);
+    }
+  },
+  signInOptions: [
+    {
+      provider: firebase.auth.EmailAuthProvider.PROVIDER_ID,
+      requireDisplayName: false
+    },
+    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+    firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+    firebase.auth.TwitterAuthProvider.PROVIDER_ID
+  ],
+  credentialHelper: firebaseui.auth.CredentialHelper.NONE
+};
+
+const ui = new firebaseui.auth.AuthUI(firebase.auth());
+ui.disableAutoSignIn();
+ui.start('#firebaseui-container', FIREBASE_UI_CONFIG);
