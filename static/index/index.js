01ba62ae6f261509cb5bed2d967ecbfc8a80384b
Stanton Parham <stparham@ncsu.edu>
2020-06-24 17:40:06 -0400

    Populate table and map with data returned from growAreaProbs/

--- static/index/index.js
+++ static/index/index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 /** The ID of the HTML element that holds the map. */
-const MAP_EL_ID = "closure-map";
+const MAP_EL_ID = 'closure-map';
 /** Options for the map. */
 const MAP_OPTIONS = {
   center: {
@@ -27,43 +27,38 @@ const MAP_OPTIONS = {
   mapTypeId: 'hybrid' // 'roadmap', 'satellite', 'hybrid', 'terrain'
 };
 /** The ID of the grow area table element. */
-const GROW_AREA_TABLE_ID = "area-table";
-
-let areaDataPromise;
-let areaData;
-(async () => {
-  areaDataPromise = getAreaData();
-  areaData = await areaDataPromise;
-  initTable();
-  initMap();
-})();
+const GROW_AREA_TABLE_ID = 'area-table';
+/** The path to the grow area bound file. */
+const GROW_AREA_BOUNDS_PATH = 'static/growing_area_bounds.geojson';
 
 /**
  * Fetches the growing area data from the server and returns it.
  */
-async function getAreaData() {
-  let res = await fetch("/areaData");
-  // if (res.ok) {
-  //   return await res.json();
-  // }
-  // console.log("Problem retrieving growing area data.");
-  // console.log(res);
-  // return null;
-  return {};
+async function getGrowAreaData() {
+  let res = await fetch('/growAreaProbs');
+  if (res.ok) {
+    return await res.json();
+  }
+  console.log('Problem retrieving growing area data.');
+  console.log(res);
+  return null;
 }
 
 /**
  * Fills the table with the growing area data.
  */
-function initTable() {
-  const table = document.getElementById(GROW_AREA_TABLE_ID).getElementsByTagName("tbody")[0];
-  for (let [areaId, data] of Object.entries(areaData)) {
-    let row = table.insertRow();
-    row.insertCell().appendChild(document.createTextNode(areaId));
-    row.insertCell().appendChild(document.createTextNode(data.prob3Day));
-    row.insertCell().appendChild(document.createTextNode(data.prob2Day));
-    row.insertCell().appendChild(document.createTextNode(data.prob1Day));
+function initGrowAreaTable(growAreaData) {
+  const rows = [];
+  for (let [areaId, data] of Object.entries(growAreaData)) {
+    const rowData = {
+      grow_area: areaId,
+      min_max_1d_prob: `${data.min_1d_prob}/${data.max_1d_prob}`,
+      min_max_2d_prob: `${data.min_2d_prob}/${data.max_2d_prob}`,
+      min_max_3d_prob: `${data.min_3d_prob}/${data.max_3d_prob}`
+    };
+    rows.push(rowData);
   }
+  $(`#${GROW_AREA_TABLE_ID}`).bootstrapTable('load', rows);
 }
 
 /**
@@ -73,46 +68,48 @@ function initTable() {
  */
 async function loadGeoJson(map, url) {
   return new Promise((resolve) => {
-    map.data.loadGeoJson(url, {idPropertyName: "grow_area"}, resolve);
+    map.data.loadGeoJson(url, {idPropertyName: 'grow_area'}, resolve);
   });
 }
 
 /**
  * Initializes the map.
  */
-async function initMap() {
+async function initMap(growAreaData) {
   const mapEl = document.getElementById(MAP_EL_ID);
   const map = new google.maps.Map(mapEl, MAP_OPTIONS);
-  await loadGeoJson(map, "static/growing_area_bounds.geojson");
-  // wait for the growing area data to become available
-  await areaDataPromise;
+  await loadGeoJson(map, GROW_AREA_BOUNDS_PATH);
 
   // set feature properties based on the gowing area data
-  for (let [areaId, data] of Object.entries(areaData)) {
+  for (let [areaId, data] of Object.entries(growAreaData)) {
     const curFeature = map.data.getFeatureById(areaId);
-    curFeature.setProperty("prob1Day", data.prob1Day);
-    curFeature.setProperty("prob2Day", data.prob2Day);
-    curFeature.setProperty("prob3Day", data.prob3Day);
+    curFeature.setProperty('min_1d_prob', data.min_1d_prob);
+    curFeature.setProperty('max_1d_prob', data.max_1d_prob);
+    curFeature.setProperty('min_2d_prob', data.min_2d_prob);
+    curFeature.setProperty('max_2d_prob', data.max_2d_prob);
+    curFeature.setProperty('min_3d_prob', data.min_3d_prob);
+    curFeature.setProperty('max_3d_prob', data.max_3d_prob);
   }
 
   // set up info windows to appear when clicking on growing areas
   const infoWindow = new google.maps.InfoWindow();
-  map.data.addListener('click', function(event) {
+  map.data.addListener('click', (event) => {
     const pos = event.latLng;
     const grow_area = event.feature.getProperty('grow_area');
-    const min_3d_prob = event.feature.getProperty('min_3d_prob');
-    const max_3d_prob = event.feature.getProperty('max_3d_prob');
-    const min_2d_prob = event.feature.getProperty('min_2d_prob');
-    const max_2d_prob = event.feature.getProperty('max_2d_prob');
     const min_1d_prob = event.feature.getProperty('min_1d_prob');
     const max_1d_prob = event.feature.getProperty('max_1d_prob');
+    const min_2d_prob = event.feature.getProperty('min_2d_prob');
+    const max_2d_prob = event.feature.getProperty('max_2d_prob');
+    const min_3d_prob = event.feature.getProperty('min_3d_prob');
+    const max_3d_prob = event.feature.getProperty('max_3d_prob');
     infoWindow.setPosition(pos);
     infoWindow.setContent(`
       <div>Area: ${grow_area}
       <br>3-day Min/Max %: ${min_3d_prob}/${max_3d_prob}
       <br>2-day Min/Max %: ${min_2d_prob}/${max_2d_prob}
       <br>1-day Min/Max %: ${min_1d_prob}/${max_1d_prob}
-      </div>`);
+      </div>
+    `);
     infoWindow.open(map);
   });
 
@@ -125,12 +122,11 @@ async function initMap() {
  * @param {google.maps.data.Feature} feature the feature to style
  */
 function styleFeature(feature) {
-  const RED_HSL = [5, 69, 54]; // red in HSL
-  // const GREEN_HSL = [151, 83, 34]; // green in HSL
-  const GREEN_HSL = [151, 83, 34]; // green in HSL
+  const RED = [255, 0, 0];
+  const WHITE = [255, 255, 255];
 
   // calculate the closure probability as a floating point
-  const prob1Day = feature.getProperty('prob1Day');
+  const prob1Day = feature.getProperty('min_1d_prob');
   let closureProbPerc = 0; // default the closure probability to 0
   if (prob1Day) {
     closureProbPerc = prob1Day / 100;
@@ -139,15 +135,15 @@ function styleFeature(feature) {
   // calculate the color of the growing area (feature)
   const areaColor = [];
   for (var i = 0; i < 3; i++) {
-    areaColor[i] = lerp(GREEN_HSL[i], RED_HSL[i], closureProbPerc);
+    areaColor[i] = lerp(WHITE[i], RED[i], closureProbPerc);
   }
 
   return {
     strokeColor: '#000',
     strokeWeight: 1,
     strokeOpacity: 0.5,
-    fillColor: 'hsl(360,100%,100%', // `hsl(${areaColor[0]},${areaColor[1]}%,${areaColor[2]}%)`,
-    fillOpacity: 0.5
+    fillColor: `rgb(${areaColor[0]},${areaColor[1]},${areaColor[2]})`,
+    fillOpacity: 0.8
   };
 }
 
@@ -160,3 +156,15 @@ function styleFeature(feature) {
 function lerp(lowVal, highVal, factor) {
   return (highVal - lowVal) * factor + lowVal;
 }
+
+(async () => {
+  const growAreaData = await getGrowAreaData();
+  initGrowAreaTable(growAreaData);
+
+  // let leaseData = {};
+  // firebase.auth().onAuthStateChanged((user) => {
+  //   user ? loadLeaseProbs(user) : () => {};
+  // });
+
+  initMap(growAreaData);
+})();
