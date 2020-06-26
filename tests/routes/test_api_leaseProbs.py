62c40fc3e370ef6d5ae2c29d937415f08d778b9f
Stanton Parham <stparham@ncsu.edu>
2020-06-26 12:12:33 -0400

    Update leaseProbs/ to include geo_boundary

--- tests/routes/test_api_leaseProbs.py
+++ tests/routes/test_api_leaseProbs.py
@@ -18,9 +18,9 @@ def test_valid(client, dbSession, addMockFbUser):
 
   # add some leases to the database
   leases = [
-    Lease(user_id=user.id, ncdmf_lease_id='45678', grow_area_name='A01', rainfall_thresh_in=1.5),
-    Lease(user_id=user.id, ncdmf_lease_id='12345', grow_area_name='B02', rainfall_thresh_in=2.5),
-    Lease(user_id=user.id, ncdmf_lease_id='82945', grow_area_name='C01', rainfall_thresh_in=1.5)
+    Lease(user_id=user.id, ncdmf_lease_id='45678', grow_area_name='A01', rainfall_thresh_in=1.5, geo_boundary='{"type": "Feature", "geometry": {"type": "Point", "coordinates": [-77.567573, 34.404497]}}'),
+    Lease(user_id=user.id, ncdmf_lease_id='12345', grow_area_name='B02', rainfall_thresh_in=2.5, geo_boundary='{"type": "Feature", "geometry": {"type": "Point", "coordinates": [-76.523872, 35.207332]}}'),
+    Lease(user_id=user.id, ncdmf_lease_id='82945', grow_area_name='C01', rainfall_thresh_in=1.5, geo_boundary='{"type": "Feature", "geometry": {"type": "Point", "coordinates": [-75.927864, 36.164344]}}')
   ]
 
   dbSession.add_all(leases)
@@ -45,9 +45,15 @@ def test_valid(client, dbSession, addMockFbUser):
   assert json[0]['prob_1d_perc'] == 60
   assert json[0]['prob_2d_perc'] == 70
   assert json[0]['prob_3d_perc'] == 80
+  assert json[0]['ncdmf_lease_id'] == '45678'
+  assert json[0]['geo_boundary'] == '{"type": "Feature", "geometry": {"type": "Point", "coordinates": [-77.567573, 34.404497]}}'
   assert json[1]['prob_1d_perc'] == 45
   assert json[1]['prob_2d_perc'] == 54
   assert json[1]['prob_3d_perc'] == 57
+  assert json[1]['ncdmf_lease_id'] == '12345'
+  assert json[1]['geo_boundary'] == '{"type": "Feature", "geometry": {"type": "Point", "coordinates": [-76.523872, 35.207332]}}'
   assert json[2]['prob_1d_perc'] == 32
   assert json[2]['prob_2d_perc'] == 33
   assert json[2]['prob_3d_perc'] == 69
+  assert json[2]['ncdmf_lease_id'] == '82945'
+  assert json[2]['geo_boundary'] == '{"type": "Feature", "geometry": {"type": "Point", "coordinates": [-75.927864, 36.164344]}}'
