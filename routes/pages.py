d8561bf2fe975bae494592fcc6e6f80b6f0fd61c
Stanton Parham <stparham@ncsu.edu>
2020-07-10 16:50:59 -0400

    Remove test route

--- routes/pages.py
+++ routes/pages.py
@@ -19,15 +19,3 @@ def preferencesPage():
 @pages.route('/signin')
 def signinPage():
   return render_template('signin.html.jinja')
-
-@pages.route('/super-special-extra-secret-test-route')
-def test():
-  print('Test print: blah blah')
-  logging.debug('Test logging.debug: superfluous stuff')
-  logging.info('Test logging.info: normal stuff')
-  logging.warning('Test logging.warning: potentially bad stuff')
-  logging.error('Test logging.error: bad stuff')
-  logging.exception('Test logging.exception: worse stuff')
-  logging.critical('Test logging.critical: absolutely terrible stuff')
-  return 'This is just a testing route.  How\'d you find this?'
-
