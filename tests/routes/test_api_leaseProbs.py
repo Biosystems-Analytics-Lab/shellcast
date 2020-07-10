c9248e41dc89b1fb1ae1dfad64dadc84b944fd9a
Stanton Parham <stparham@ncsu.edu>
2020-07-10 16:45:30 -0400

    Add more tests for /leaseProbs

--- tests/routes/test_api_leaseProbs.py
+++ tests/routes/test_api_leaseProbs.py
@@ -57,3 +57,65 @@ def test_valid(client, dbSession, addMockFbUser):
   assert json[2]['prob_3d_perc'] == 69
   assert json[2]['ncdmf_lease_id'] == '82945'
   assert json[2]['geo_boundary'] == '{"type": "Feature", "geometry": {"type": "Point", "coordinates": [-75.927864, 36.164344]}}'
+
+def test_no_probabilities(client, dbSession, addMockFbUser):
+  # add a mock Firebase user
+  addMockFbUser(dict(uid='3sH9so5Y3DP72QA1XqbWw9J6I8o1', email='blah@gmail.com', phone_number='11234567890'), 'validUser1')
+
+  # add the user to the db
+  user = User(firebase_uid='3sH9so5Y3DP72QA1XqbWw9J6I8o1', email='blah@gmail.com', phone_number='11234567890')
+
+  dbSession.add(user)
+  dbSession.commit()
+
+  # add some leases to the database
+  leases = [
+    Lease(user_id=user.id, ncdmf_lease_id='45678', grow_area_name='A01', rainfall_thresh_in=1.5, geo_boundary='{"type": "Feature", "geometry": {"type": "Point", "coordinates": [-77.567573, 34.404497]}}'),
+    Lease(user_id=user.id, ncdmf_lease_id='12345', grow_area_name='B02', rainfall_thresh_in=2.5, geo_boundary='{"type": "Feature", "geometry": {"type": "Point", "coordinates": [-76.523872, 35.207332]}}'),
+    Lease(user_id=user.id, ncdmf_lease_id='82945', grow_area_name='C01', rainfall_thresh_in=1.5, geo_boundary='{"type": "Feature", "geometry": {"type": "Point", "coordinates": [-75.927864, 36.164344]}}')
+  ]
+
+  dbSession.add_all(leases)
+  dbSession.commit()
+
+  lease1Prob = ClosureProbability(lease_id=leases[1].id, prob_1d_perc=45, prob_2d_perc=54, prob_3d_perc=57)
+  dbSession.add(lease1Prob)
+  dbSession.commit()
+
+  res = client.get('/leaseProbs', headers={'Authorization': 'validUser1'})
+  assert res.status_code == 200
+
+  json = res.get_json()
+  assert len(json) == 3
+
+  assert json[0].get('prob_1d_perc') == None
+  assert json[0].get('prob_2d_perc') == None
+  assert json[0].get('prob_3d_perc') == None
+  assert json[0]['ncdmf_lease_id'] == '45678'
+  assert json[0]['geo_boundary'] == '{"type": "Feature", "geometry": {"type": "Point", "coordinates": [-77.567573, 34.404497]}}'
+  assert json[1].get('prob_1d_perc') == 45
+  assert json[1].get('prob_2d_perc') == 54
+  assert json[1].get('prob_3d_perc') == 57
+  assert json[1]['ncdmf_lease_id'] == '12345'
+  assert json[1]['geo_boundary'] == '{"type": "Feature", "geometry": {"type": "Point", "coordinates": [-76.523872, 35.207332]}}'
+  assert json[2].get('prob_1d_perc') == None
+  assert json[2].get('prob_2d_perc') == None
+  assert json[2].get('prob_3d_perc') == None
+  assert json[2]['ncdmf_lease_id'] == '82945'
+  assert json[2]['geo_boundary'] == '{"type": "Feature", "geometry": {"type": "Point", "coordinates": [-75.927864, 36.164344]}}'
+
+def test_no_leases(client, dbSession, addMockFbUser):
+  # add a mock Firebase user
+  addMockFbUser(dict(uid='3sH9so5Y3DP72QA1XqbWw9J6I8o1', email='blah@gmail.com', phone_number='11234567890'), 'validUser1')
+
+  # add the user to the db
+  user = User(firebase_uid='3sH9so5Y3DP72QA1XqbWw9J6I8o1', email='blah@gmail.com', phone_number='11234567890')
+
+  dbSession.add(user)
+  dbSession.commit()
+
+  res = client.get('/leaseProbs', headers={'Authorization': 'validUser1'})
+  assert res.status_code == 200
+
+  json = res.get_json()
+  assert len(json) == 0
