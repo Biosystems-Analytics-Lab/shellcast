d422bb9d969e8b08342b171685408e57019cca69
Stanton Parham <stparham@ncsu.edu>
2020-07-10 17:27:44 -0400

    Add more tests for /userInfo

--- tests/routes/test_api_userInfo.py
+++ tests/routes/test_api_userInfo.py
@@ -53,3 +53,55 @@ def test_expiredToken(client, addMockFbUser):
 
   res = client.get('/userInfo', headers={'Authorization': 'validUser1'})
   assert res.status_code == 401
+
+def test_update_info(client, addMockFbUser):
+  # add a mock Firebase user
+  addMockFbUser(dict(uid='blah', email='blah@gmail.com', phone_number='11234567890'), 'validUser1')
+
+  # request user info
+  res = client.get('/userInfo', headers={'Authorization': 'validUser1'})
+  assert res.status_code == 200
+  json = res.get_json()
+  assert json['phone_number'] == '11234567890'
+  assert json['email'] == 'blah@gmail.com'
+
+  # make a request to update the user's info
+  res = client.post('/userInfo', headers={'Authorization': 'validUser1'}, json={'email': 'secondemail@gmail.com', 'phone_number': '5555555555'})
+  assert res.status_code == 200
+  
+  # check that the user's info is updated
+  res = client.get('/userInfo', headers={'Authorization': 'validUser1'})
+  assert res.status_code == 200
+  json = res.get_json()
+  assert json['phone_number'] == '5555555555'
+  assert json['email'] == 'secondemail@gmail.com'
+
+def test_update_invalid_email(client, addMockFbUser):
+  # add a mock Firebase user
+  addMockFbUser(dict(uid='blah', email='blah@gmail.com', phone_number='11234567890'), 'validUser1')
+
+  # make a request to update the user's info with an invalid email
+  res = client.post('/userInfo', headers={'Authorization': 'validUser1'}, json={'email': 'myemail'})
+  assert res.status_code == 400
+  
+  # check that the user's info is not updated
+  res = client.get('/userInfo', headers={'Authorization': 'validUser1'})
+  assert res.status_code == 200
+  json = res.get_json()
+  assert json['phone_number'] == '11234567890'
+  assert json['email'] == 'blah@gmail.com'
+
+def test_update_invalid_phone(client, addMockFbUser):
+  # add a mock Firebase user
+  addMockFbUser(dict(uid='blah', email='blah@gmail.com', phone_number='11234567890'), 'validUser1')
+
+  # make a request to update the user's info with an invalid email
+  res = client.post('/userInfo', headers={'Authorization': 'validUser1'}, json={'phone_number': '123456789'})
+  assert res.status_code == 400
+  
+  # check that the user's info is not updated
+  res = client.get('/userInfo', headers={'Authorization': 'validUser1'})
+  assert res.status_code == 200
+  json = res.get_json()
+  assert json['phone_number'] == '11234567890'
+  assert json['email'] == 'blah@gmail.com'
