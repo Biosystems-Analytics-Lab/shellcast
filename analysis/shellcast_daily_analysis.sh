5f98ab8afe644bf715e637585afada68a68604eb
sheila <sms493@cornell.edu>
2020-07-29 22:54:15 -0400

    fixed cronjob plist script

--- analysis/shellcast_daily_analysis.sh
+++ analysis/shellcast_daily_analysis.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 # script name: shellcast_analysis.sh
 # purpose of script: this bash script runs python and r scripts for shellcast analysis and save outputs to terminal_data output folder
 # author: sheila saia
@@ -7,10 +9,10 @@
 # NOTE! need to define folder output paths below
 
 # step 1
-python3 ndfd_get_forecast_data_script.py | tee ...data/tabular/output/terminal_data/01_python_output_$(date '+%Y%m%d').txt
+opt/anaconda3/bin/python ndfd_get_forecast_data_script.py | tee opt/analysis/data/tabular/output/terminal_data/01_python_output_$(date '+%Y%m%d').txt
 
 # step 2
-Rscript ndfd_convert_df_to_raster_script.R | tee ...data/tabular/outputs/terminal_data/02_convert_df_out_$(date '+%Y%m%d').txt
+/usr/local/bin/Rscript ndfd_convert_df_to_raster_script.R | tee opt/analysis/data/tabular/outputs/terminal_data/02_convert_df_out_$(date '+%Y%m%d').txt
 
 # step 3
-Rscript ndfd_analyze_forecast_data_script.R | tee ...data/tabular/outputs/terminal_data/03_analyze_out_$(date '+%Y%m%d').txt
+/usr/local/bin/Rscript ndfd_analyze_forecast_data_script.R | tee opt/analysis/data/tabular/outputs/terminal_data/03_analyze_out_$(date '+%Y%m%d').txt
