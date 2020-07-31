3c53bc6efa9082dc7fb9ed5e3e52567f3ada82cd
Stanton Parham <stparham@ncsu.edu>
2020-07-31 14:05:24 -0400

    Add foreign key for service provider in users table

--- models/User.py
+++ models/User.py
@@ -6,12 +6,14 @@ class User(db.Model):
   __tablename__ = 'users'
 
   id = db.Column(db.Integer, primary_key=True)
+  service_provider_id = db.Column(db.Integer, db.ForeignKey('phone_service_providers.id'))
   firebase_uid = db.Column(db.String(28))
   phone_number = db.Column(db.String(11))
   email = db.Column(db.String(50))
   created = db.Column(db.DateTime, server_default=db.func.now())
   updated = db.Column(db.DateTime, server_default=db.func.now(), onupdate=db.func.now())
 
+  service_provider = db.relationship('PhoneServiceProvider', back_populates='users')
   leases = db.relationship('Lease', order_by=Lease.created, back_populates='user')
   notifications = db.relationship('Notification', order_by=Notification.created, back_populates='user')
 
