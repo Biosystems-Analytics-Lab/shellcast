dea85fbcd453f9eb61301d1951429490054bb44d
sheila <sms493@cornell.edu>
2020-08-03 17:38:28 -0400

    minor edits

--- analysis/ndfd_get_forecast_data_script.py
+++ analysis/ndfd_get_forecast_data_script.py
@@ -39,13 +39,13 @@ from csv import writer
 # %% set paths here
 
 # base path to analysis
-# analysis_base_path = ".../analysis/" # set this and uncomment!
-analysis_base_path = "/Users/sheila/Documents/github/shellcast-analysis/"
+analysis_base_path = "opt/analysis/" # set this and uncomment!
+# analysis_base_path = "/Users/sheila/Documents/github/shellcast-analysis/"
 
 # base path to data
-data_base_path = "opt/shellcast/analysis/data/" # set this and uncomment!
+# data_base_path = "opt/shellcast/analysis/data/" # set this and uncomment!
 # data_base_path = "/Users/sheila/Documents/bae_shellcast_project/shellcast_analysis/web_app_data/"
-
+data_base_path = "/Users/sheila/Documents/github_ncsu/shellcast/analysis/data/"
 
 # %% use set paths
 
