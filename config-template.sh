e800861a1082c371b0792eda71266234bf640ecf
sheila <sms493@cornell.edu>
2020-08-05 11:47:08 -0400

    initial commit shell config template

---
+++ config-template.sh
@@ -0,0 +1,6 @@
+# the name of the Cloud SQL instance to connect to
+CLOUD_SQL_INSTANCE_NAME = 'ncsu-shellcast:us-east1:ncsu-shellcast-database'
+# the path to the analysis directory
+ANALYSIS_PATH = 'ADD_VALUE_HERE'
+# the path to the data directory
+DATA_PATH = 'ADD_VALUE_HERE' # could also use concat here "${str1}${str2}"
