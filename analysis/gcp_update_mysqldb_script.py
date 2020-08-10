a7d9551758c0d5f65671c7d85c08ddb3a6e22c61
sheila <sms493@cornell.edu>
2020-08-10 18:43:52 -0400

    fixed null issue in db

--- analysis/gcp_update_mysqldb_script.py
+++ analysis/gcp_update_mysqldb_script.py
@@ -2,7 +2,7 @@
 """
 # ---- script header ----
 script name: gcp_update_mysqldb_script.py
-purpose of script: This script updates the ShellCast MySQL database. Specifically it updates 
+purpose of script: This script updates the ShellCast MySQL database. Specifically it updates
 the sga_min_max table, the ncdmf_leases table, and the closure_probabilities table.
 email: ssaia@ncsu.edu
 date created: 20200716
@@ -14,25 +14,20 @@ notes:
 help:
 pymysql help: https://github.com/PyMySQL/PyMySQL
 pymysql docs: https://pymysql.readthedocs.io/en/latest/
-gcp docs: https://cloud.google.com/sql/docs/mysql/connect-app-engine-standard
-
-# install geopandas (in the shell)
-conda install -c conda-forge geopandas
-
-# install geoalchemy2 (in the shell)
-conda install -c conda-forge geoalchemy2
-
 
 """
 
 # %% to do list
 
-# TODO update leases
 
+# %% start script
+
+print("starting gcp mysql db update")
 
 # %% load libraries
 
 import pandas
+# import numpy
 import pymysql
 # from sqlalchemy import create_engine
 import sqlalchemy
@@ -123,15 +118,15 @@ connection = pymysql.connect(host = DevConfig.HOST,
 
 # %% update sga min and max table
 
-sga_data = sga_data[1:5]
+# sga_data = sga_data[1:5] # for testing
 
 # add df to mysql db
-sga_data.to_sql('sga_min_max', engine, if_exists = 'append', index = False)
+# sga_data.to_sql('sga_min_max', engine, if_exists = 'append', index = False)
 
 # print status
 print("added sga min and max data to mysql db")
 # print("did not add sga min and max data to mysql db")
- 
+
 # create cursor to print out status of update
 # sga_cursor = connection.cursor()
 # execute query
@@ -163,46 +158,39 @@ ncdmf_leases_current_result = ncdmf_lease_cursor.fetchall()
 if (len(ncdmf_leases_current_result) > 0):
     # convert to pandas df
     ncdmf_leases_current_df = pandas.DataFrame(ncdmf_leases_current_result)
-    
+
     # get ncdmf lease ids
     ncdmf_leases_current_ids = ncdmf_leases_current_df['ncdmf_lease_id']
-    
-    # anti-join to find new ncdmf leases from the ncdmf rest api 
+
+    # anti-join to find new ncdmf leases from the ncdmf rest api
     # (i.e., NOT in ncdmf_leases_current_ids and NOT in ncdmf_leases shellcast mysql table)
     lease_spatial_data_sel = lease_spatial_data[~lease_spatial_data['ncdmf_lease_id'].isin(ncdmf_leases_current_ids)].reset_index(drop=True)
-    
+
     lease_spatial_data_sel = lease_spatial_data_sel[1:6] # add the first five for now
-    
+
     # if there are no new leases to add (from the ncdmf rest api) then skip inserting rows
     if (len(lease_spatial_data_sel) > 0):
-        
+
         # get sql query
         ncdmf_leases_insert_query = make_lease_sql_query(lease_spatial_data_sel)
-        
+
         # execute query
-        ncdmf_lease_cursor.execute(ncdmf_leases_insert_query)
-        
+        # ncdmf_lease_cursor.execute(ncdmf_leases_insert_query)
+
         # commit changes to remote db
-        connection.commit()
-        
+        # connection.commit()
+
         # print when finished
         print("added ncdmf lease data to mysql db")
-        
+
     else:
         # print when finished
         print("there were no new ncdmf leases to add to mysql db")
-        
+
 else:
     # print that database is empty
     print("the database is empty so leases cannot be added")
-
-#%%
-#ncdmf_leases_insert_query
-
-#"INSERT INTO `ncdmf_leases` (`ncdmf_lease_id`, `grow_area_name`, `rainfall_thresh_in`, `geometry`)
-#VALUES 
-#('443', 'E09', 2.0, ST_PointFromText('POINT(-76.391809 34.874979)')), 
-#('1259571', 'E06', 3.0, ST_PointFromText('POINT(-76.550947 34.701303)'));"
+    
 
 # %% update closure_probabilities table
 
@@ -213,34 +201,78 @@ user_leases_cursor = connection.cursor()
 user_leases_current_sql = "SELECT id, ncdmf_lease_id FROM user_leases"
 user_leases_cursor.execute(user_leases_current_sql)
 
-# save result
+# save current lease result
 user_leases_current_result = user_leases_cursor.fetchall()
 
-# convert to pandas df
+# convert to curent lease result to pandas df
 user_leases_current_df = pandas.DataFrame(user_leases_current_result)
 
-# get ncdmf lease ids
-user_leases_current_ncdmf_ids = user_leases_current_df['ncdmf_lease_id']
-user_leases_current_db_ids = user_leases_current_df['id']
+# lease calcs df select only needed columns 
+lease_data_sel = lease_data.drop(columns = 'day').reset_index(drop=True)
 
-# find lease results for leases in db
-user_leases_data_sel_init = lease_data[lease_data.lease_id.isin(user_leases_current_ncdmf_ids)]
-# lease_data_sel_init = lease_data[0:3] # for testing
+# rename to match current lease df
+lease_data_sel.columns = ['ncdmf_lease_id', 'prob_1d_perc', 'prob_2d_perc', 'prob_3d_perc']
 
-# final cleaned up version of lease_dato push to db
-user_leases_data_sel = user_leases_data_sel_init.drop(columns = 'day').reset_index(drop=True)
+# set index of lease_data_sel
+lease_data_sel_fix = lease_data_sel.set_index('ncdmf_lease_id')
 
-# use lease data to create closure probabilities dataset
-closure_prob_data = pandas.DataFrame({'lease_id' : user_leases_current_db_ids, # key is actually id column
-                                     'prob_1d_perc' : user_leases_data_sel['prob_1d_perc'],
-                                     'prob_2d_perc' : user_leases_data_sel['prob_2d_perc'],
-                                     'prob_3d_perc' : user_leases_data_sel['prob_3d_perc']})
+# join lease_data_sel to user_leases_current_df
+leases_join_df = user_leases_current_df.set_index(
+        'ncdmf_lease_id').join(
+                lease_data_sel_fix, how = 'left', on = 'ncdmf_lease_id').set_index(
+                        'id').reset_index(drop = False)
 
+# finalize data to add to mysql db
+closure_prob_data = pandas.DataFrame({'lease_id' : leases_join_df['id'],
+                                     'prob_1d_perc' : leases_join_df['prob_1d_perc'],
+                                     'prob_2d_perc' : leases_join_df['prob_2d_perc'],
+                                     'prob_3d_perc' : leases_join_df['prob_3d_perc']})
+    
 # add df to mysql db
 closure_prob_data.to_sql('closure_probabilities', engine, if_exists = 'append', index = False)
 
 # print status
 print("added user lease data to mysql db")
+    
+# to_sql can handle NaN to NULL conversion!
+# in most cases there should not be NULL values
+# but for testing some of the lease id's were made up
+
+
+# %% dispose engine and close connection
+
+engine.dispose()
+connection.close()
+
+# print status
+print("gcp connection and engine closed")
+
+
+# %% extra/draft code
+
+# final fixing up (replacing nan with null)
+# closure_prob_data = leases_join_df.replace(numpy.NAN, sqlalchemy.sql.null)
+
+# get ncdmf lease ids
+# user_leases_current_ncdmf_ids = user_leases_current_df['ncdmf_lease_id']
+# user_leases_current_db_ids = user_leases_current_df['id']
+
+# find lease results for leases in db
+# user_leases_data_sel_init = lease_data[lease_data.lease_id.isin(user_leases_current_ncdmf_ids)]
+# lease_data_sel_init = lease_data[0:3] # for testing
+
+# find lease id for leases in db
+# user_leases_current_db_ids = user_leases_current_df['id']
+#user_leases_current_db_ids_sel =user_leases_current_db_ids[user_leases_current_df.ncdmf_lease_id.isin(user_leases_data_sel_init.lease_id)]
+
+# final cleaned up version of lease_dato push to db
+# user_leases_data_sel = user_leases_data_sel_init.drop(columns = 'day').reset_index(drop=True)
+
+# use lease data to create closure probabilities dataset
+#closure_prob_data = pandas.DataFrame({'lease_id' : user_leases_current_db_ids, # key is actually id column
+#                                     'prob_1d_perc' : user_leases_data_sel['prob_1d_perc'],
+#                                     'prob_2d_perc' : user_leases_data_sel['prob_2d_perc'],
+#                                     'prob_3d_perc' : user_leases_data_sel['prob_3d_perc']})
 
 # create cursor to print out status of update
 # lease_cursor = connection.cursor()
@@ -252,12 +284,3 @@ print("added user lease data to mysql db")
 # lease_result = lease_cursor.fetchall()
 # for i in lease_result:
 #    print(i)
-
-
-# %% dispose engine and close connection
-
-engine.dispose()
-connection.close()
-
-# print status
-print("gcp connection and engine closed")
