f26f61441d99d9e43d3ec5f4c1cd4d01a81e6162
Stanton Parham <stparham@ncsu.edu>
2020-08-13 16:37:26 -0400

    Use direct imports from sqlalchemy for ORM models

--- models/ClosureProbability.py
+++ models/ClosureProbability.py
@@ -1,17 +1,21 @@
+from sqlalchemy import Column, Integer, DateTime, ForeignKey
+from sqlalchemy.sql import functions
+from sqlalchemy.orm import relationship
+
 from models import db
 from models.Notification import Notification
 
 class ClosureProbability(db.Model):
   __tablename__ = 'closure_probabilities'
-  id = db.Column(db.Integer, primary_key=True)
-  lease_id = db.Column(db.Integer, db.ForeignKey('user_leases.id'))
-  prob_1d_perc = db.Column(db.Integer)
-  prob_2d_perc = db.Column(db.Integer)
-  prob_3d_perc = db.Column(db.Integer)
-  created = db.Column(db.DateTime, server_default=db.func.now())
-  updated = db.Column(db.DateTime, server_default=db.func.now(), onupdate=db.func.now())
+  id = Column(Integer, primary_key=True)
+  lease_id = Column(Integer, ForeignKey('user_leases.id'))
+  prob_1d_perc = Column(Integer)
+  prob_2d_perc = Column(Integer)
+  prob_3d_perc = Column(Integer)
+  created = Column(DateTime, server_default=functions.now())
+  updated = Column(DateTime, server_default=functions.now(), onupdate=functions.now())
 
-  lease = db.relationship('Lease', back_populates='closureProbabilities')
+  lease = relationship('Lease', back_populates='closureProbabilities')
 
   def asDict(self):
     return {
--- models/Lease.py
+++ models/Lease.py
@@ -1,27 +1,29 @@
+from sqlalchemy import Column, Integer, Float, String, Boolean, DateTime, ForeignKey
+from sqlalchemy.sql import functions, expression
+from sqlalchemy.orm import relationship
+
 from models import db
 from models.ClosureProbability import ClosureProbability
 from models.PointColType import PointColType
 
-from sqlalchemy.sql import expression
-
 class Lease(db.Model):
   __tablename__ = 'user_leases'
 
-  id = db.Column(db.Integer, primary_key=True)
-  user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
-  ncdmf_lease_id = db.Column(db.String(20))
-  grow_area_name = db.Column(db.String(3))
-  rainfall_thresh_in = db.Column(db.Float)
-  geometry = db.Column(PointColType)
-  email_pref = db.Column(db.Boolean, server_default=expression.false(), default=False)
-  text_pref = db.Column(db.Boolean, server_default=expression.false(), default=False)
-  prob_pref = db.Column(db.Integer, server_default=expression.literal(75), default=75)
-  deleted_by_user = db.Column(db.Boolean, server_default=expression.false(), default=False)
-  created = db.Column(db.DateTime, server_default=db.func.now())
-  updated = db.Column(db.DateTime, server_default=db.func.now(), onupdate=db.func.now())
+  id = Column(Integer, primary_key=True)
+  user_id = Column(Integer, ForeignKey('users.id'))
+  ncdmf_lease_id = Column(String(20))
+  grow_area_name = Column(String(3))
+  rainfall_thresh_in = Column(Float)
+  geometry = Column(PointColType)
+  email_pref = Column(Boolean, server_default=expression.false(), default=False)
+  text_pref = Column(Boolean, server_default=expression.false(), default=False)
+  prob_pref = Column(Integer, server_default=expression.literal(75), default=75)
+  deleted_by_user = Column(Boolean, server_default=expression.false(), default=False)
+  created = Column(DateTime, server_default=functions.now())
+  updated = Column(DateTime, server_default=functions.now(), onupdate=functions.now())
 
-  user = db.relationship('User', back_populates='leases')
-  closureProbabilities = db.relationship('ClosureProbability', order_by=ClosureProbability.id.desc(), back_populates='lease')
+  user = relationship('User', back_populates='leases')
+  closureProbabilities = relationship('ClosureProbability', order_by=ClosureProbability.id.desc(), back_populates='lease')
 
   def asDict(self):
     return {
