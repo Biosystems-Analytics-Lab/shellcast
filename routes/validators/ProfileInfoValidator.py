98af012e30d71e5b0a8e25a4b4604342769b52c3
Stanton Parham <stparham@ncsu.edu>
2020-09-01 15:58:33 -0400

    Improve validator error messages

--- routes/validators/ProfileInfoValidator.py
+++ routes/validators/ProfileInfoValidator.py
@@ -5,6 +5,14 @@ from models import db
 from models.PhoneServiceProvider import PhoneServiceProvider
 
 class ProfileInfoValidator():
+  """
+  Validates form data submitted for profile information.
+  NOTE: This validator assumes that a complete set of the data is given for
+  each field.  i.e. If there are 3 fields (email, phone number, service provider),
+  the current values for each field must be given even if only one field is 
+  actually being updated.
+  """
+
   def __init__(self, json):
     self.email = json.get('email')
     self.phone_number = json.get('phone_number')
@@ -17,35 +25,36 @@ class ProfileInfoValidator():
     return False
 
   def validate(self):
-    validators = [self.validate_email, self.validate_phone_number, self.validate_service_provider_id]
+    validators = [self.validateEmail, self.validatePhoneNumber, self.validateServiceProviderId]
+    valid = True
     for validator in validators:
       if (not validator()):
-        return False
-    return True
+        valid = False
+    return valid
 
-  def validate_email(self):
+  def validateEmail(self):
     if (not self.email):
       return self.addError('An email address is always required.')
     try:
-      valid = validate_email(self.email)
-      self.email = valid.email # Update with the normalized form.
+      validatedEmail = validate_email(self.email)
+      self.email = validatedEmail.email # Update with the normalized form.
     except EmailNotValidError as e:
       print(str(e))
       return self.addError('Email is not valid.')
     return True
 
-  def validate_phone_number(self):
+  def validatePhoneNumber(self):
     if (not self.phone_number):
       self.phone_number = None
       self.service_provider_id = None
       return True
-    if (self.phone_number and self.service_provider_id == None):
-      return self.addError('When providing a phone number, a service provider is required.')
     if (not re.search(r'^\d{10}$', self.phone_number)):
-      return self.addError('Phone number must be 10 digits.')
+      return self.addError('The phone number must be 10 digits long.')
+    if (self.phone_number and not self.service_provider_id):
+      return self.addError('When providing a phone number, a service provider is required.')
     return True
 
-  def validate_service_provider_id(self):
+  def validateServiceProviderId(self):
     if (not self.service_provider_id and not self.phone_number):
       self.phone_number = None
       self.service_provider_id = None
