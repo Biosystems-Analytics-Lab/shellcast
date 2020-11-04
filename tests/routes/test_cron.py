a8d03af61e0c28f44319998683017cfc1c7fe45c
Stanton Parham <stparham@ncsu.edu>
2020-11-04 15:14:08 -0500

    Update /sendNotifications to use overall preference data

--- tests/routes/test_cron.py
+++ tests/routes/test_cron.py
@@ -101,15 +101,15 @@ def test_sendVariedSizeTextNotifications(app, monkeyPatchBotoClient, genRandomSt
 
 def test_sendNotifications(client, dbSession, monkeyPatchBotoClient):
   # add a user to the db
-  user = User(firebase_uid='3sH9so5Y3DP72QA1XqbWw9J6I8o1', email='shellcastapp@ncsu.edu')
+  user = User(firebase_uid='3sH9so5Y3DP72QA1XqbWw9J6I8o1', email='shellcastapp@ncsu.edu', email_pref=True, prob_pref=50)
   dbSession.add(user)
   dbSession.commit()
 
   # add a few leases for the user
   leases = [
-    Lease(user_id=user.id, ncdmf_lease_id='45678', grow_area_name='A01', rainfall_thresh_in=1.5, geometry=(34.404497, -77.567573), email_pref=True, prob_pref=50),
-    Lease(user_id=user.id, ncdmf_lease_id='12345', grow_area_name='B02', rainfall_thresh_in=2.5, geometry=(35.207332, -76.523872), email_pref=True, prob_pref=75),
-    Lease(user_id=user.id, ncdmf_lease_id='82945', grow_area_name='C01', rainfall_thresh_in=1.5, geometry=(36.164344, -75.927864), email_pref=True, prob_pref=50)
+    Lease(user_id=user.id, ncdmf_lease_id='45678', grow_area_name='A01', rainfall_thresh_in=1.5, geometry=(34.404497, -77.567573)),
+    Lease(user_id=user.id, ncdmf_lease_id='12345', grow_area_name='B02', rainfall_thresh_in=2.5, geometry=(35.207332, -76.523872)),
+    Lease(user_id=user.id, ncdmf_lease_id='82945', grow_area_name='C01', rainfall_thresh_in=1.5, geometry=(36.164344, -75.927864))
   ]
   dbSession.add_all(leases)
   dbSession.commit()
@@ -117,7 +117,7 @@ def test_sendNotifications(client, dbSession, monkeyPatchBotoClient):
   # add some closure probabilities for those leases
   probabilities = [
     ClosureProbability(lease_id=leases[0].id, prob_1d_perc=60, prob_2d_perc=70, prob_3d_perc=80),
-    ClosureProbability(lease_id=leases[1].id, prob_1d_perc=45, prob_2d_perc=54, prob_3d_perc=57),
+    ClosureProbability(lease_id=leases[1].id, prob_1d_perc=45, prob_2d_perc=49, prob_3d_perc=49),
     ClosureProbability(lease_id=leases[2].id, prob_1d_perc=32, prob_2d_perc=33, prob_3d_perc=69)
   ]
   dbSession.add_all(probabilities)
