20e8b108f7badb748419d8c342222e07ad84c70a
sheilasaia <sms493@cornell.edu>
2021-04-22 15:08:49 -0400

    fixed #122

--- analysis/shellcast_daily_analysis.sh
+++ analysis/shellcast_daily_analysis.sh
@@ -8,21 +8,24 @@
 
 # NOTE! need to define folder output paths below
 
+# source config.sh file (in analysis directory)
+source config.sh
+
 # make sure that all spawned processes are killed on exit, a kill signal, or an error
 trap "exit" INT TERM ERR
 trap "kill 0" EXIT
 
 # step 1
 # opt/anaconda3/bin/python ndfd_get_forecast_data_script.py | tee opt/analysis/data/tabular/outputs/terminal_data/01_python_output_$(date '+%Y%m%d').txt
-/Users/sheila/opt/anaconda3/bin/python ndfd_get_forecast_data_script.py | tee /Users/sheila/Documents/github_ncsu/shellcast/analysis/data/tabular/outputs/terminal_data/01_get_forecast_output_$(date '+%Y%m%d').txt
+${PYTHON_PATH}python ndfd_get_forecast_data_script.py | tee ${OUTPUT_PATH}01_get_forecast_output_$(date '+%Y%m%d').txt
 
 # step 2
 # /usr/local/bin/Rscript ndfd_convert_df_to_raster_script.R | tee opt/analysis/data/tabular/outputs/terminal_data/02_convert_df_out_$(date '+%Y%m%d').txt
-/usr/local/bin/Rscript ndfd_convert_df_to_raster_script.R | tee /Users/sheila/Documents/github_ncsu/shellcast/analysis/data/tabular/outputs/terminal_data/02_convert_df_out_$(date '+%Y%m%d').txt
+${RSCRIPT_PATH}Rscript ndfd_convert_df_to_raster_script.R | tee ${OUTPUT_PATH}02_convert_df_out_$(date '+%Y%m%d').txt
 
 # step 3
 # /usr/local/bin/Rscript ndfd_analyze_forecast_data_script.R | tee opt/analysis/data/tabular/outputs/terminal_data/03_analyze_out_$(date '+%Y%m%d').txt
-/usr/local/bin/Rscript ndfd_analyze_forecast_data_script.R | tee /Users/sheila/Documents/github_ncsu/shellcast/analysis/data/tabular/outputs/terminal_data/03_analyze_out_$(date '+%Y%m%d').txt
+${RSCRIPT_PATH}Rscript ndfd_analyze_forecast_data_script.R | tee ${OUTPUT_PATH}03_analyze_out_$(date '+%Y%m%d').txt
 
 # See main README for details on thow to set up both the TCP connection.
 
@@ -30,7 +33,7 @@ trap "kill 0" EXIT
 # gcloud auth application-default login --client-id-file=/Users/sheila/.config/gcloud/application_default_credentials.json # this is not working either
 
 # open TCP connection for step 4
-/Users/sheila/cloud_sql_proxy -instances=ncsu-shellcast:us-east1:ncsu-shellcast-database=tcp:3306 &
+${CLOUD_SQL_PATH} -instances=${CLOUD_SQL_INSTANCE_NAME}=tcp:${CLOUD_SQL_PORT} &
 
 PID1=$!
 
@@ -41,7 +44,7 @@ sleep 5s
 
 # step 4
 # opt/anaconda3/bin/python gcp_update_mysqldb_script.py | tee opt/analysis/data/tabular/outputs/terminal_data/04_update_db_out_$(date '+%Y%m%d').txt
-/Users/sheila/opt/anaconda3/bin/python gcp_update_mysqldb_script.py | tee /Users/sheila/Documents/github_ncsu/shellcast/analysis/data/tabular/outputs/terminal_data/04_update_db_out_$(date '+%Y%m%d').txt
+${PYTHON_PATH}python gcp_update_mysqldb_script.py | tee ${OUTPUT_PATH}04_update_db_out_$(date '+%Y%m%d').txt
 
 # close connections
 kill -INT $PID1 #-SIGINT doesn't work here (only in the terminal manually)
--- config-template.sh
+++ config-template.sh
@@ -1,6 +1,19 @@
+# DO **NOT** INCLUDE API KEYS HERE! (VARIABLES MAY BE PRINTED TO OUTPUT OR ERROR FILES)
+# path to cloud_sql_proxy file
+CLOUD_SQL_PATH="ADD_VALUE_HERE"
+# for example "../cloud_sql_proxy"
 # the name of the Cloud SQL instance to connect to
-CLOUD_SQL_INSTANCE_NAME = 'ncsu-shellcast:us-east1:ncsu-shellcast-database'
-# the path to the analysis directory
-ANALYSIS_PATH = 'ADD_VALUE_HERE'
-# the path to the data directory
-DATA_PATH = 'ADD_VALUE_HERE' # could also use concat here "${str1}${str2}"
+CLOUD_SQL_INSTANCE_NAME="ncsu-shellcast:us-east1:ncsu-shellcast-database"
+# cloud sql port number
+CLOUD_SQL_PORT="3306"
+# path to python3 version
+PYTHON_PATH="ADD_VALUE_HERE"
+# for example "../opt/anaconda3/bin/"
+# if pthon <file name> works then leave this empty as ""
+# path to r version
+RSCRIPT_PATH="ADD_VALUE_HERE"
+# for example "../local/bin/"
+# if pthon <file name> works then leave this empty as ""
+# path to shellcast output folder (i.e., analysis > data > tabular > outputs > terminal_data)
+OUTPUT_PATH="ADD_VALUE_HERE"
+# for example "../shellcast/analysis/data/tabular/outputs/terminal_data/"
