2e148b961eb4b2ff6a019cb16cf4e891a198abb2
sheilasaia <sms493@cornell.edu>
2021-05-05 14:25:04 -0400

    fixed #121 and fixed #107

--- config-template.sh
+++ config-template.sh
@@ -8,12 +8,12 @@ CLOUD_SQL_INSTANCE_NAME="ncsu-shellcast:us-east1:ncsu-shellcast-database"
 CLOUD_SQL_PORT="3306"
 # path to python3 version
 PYTHON_PATH="ADD_VALUE_HERE"
-# for example "../opt/anaconda3/bin/"
+# for example "../opt/anaconda3/bin/" or ""
 # if pthon <file name> works then leave this empty as ""
 # path to r version
 RSCRIPT_PATH="ADD_VALUE_HERE"
-# for example "../local/bin/"
-# if pthon <file name> works then leave this empty as ""
+# we recommend "/usr/local/bin/" 
+# for some reason the plist file wasn't working if we used another path 
 # path to shellcast output folder (i.e., analysis > data > tabular > outputs > terminal_data)
 OUTPUT_PATH="ADD_VALUE_HERE"
-# for example "../shellcast/analysis/data/tabular/outputs/terminal_data/"
+# we recommend "../shellcast/analysis/data/tabular/outputs/terminal_data/"
