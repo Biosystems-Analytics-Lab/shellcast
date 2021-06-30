8a27ef4e8e7fb7d6254ee064cd8bd889d9d8d15c
EshwarCVS <thedla.ecvs@gmail.com>
2021-06-30 16:45:10 -0700

    Updated with the comments in all the files Ready for execution

--- static/index/index.js
+++ static/index/index.js
@@ -34,26 +34,19 @@ const LEASE_TABLE_ID = 'lease-table';
 const GROWING_UNIT_BOUNDS_PATH = 'static/cmu_bounds.geojson';
 /** The color used to fill in growing units without a closure probability. */
 const COLOR_NULL = 'transparent';
-// /** The color used to fill in growing units with a 0-40% closure probability. */
-// const COLOR_0_40 = '#4eb265';
-// /** The color used to fill in growing units with a 40-60% closure probability. */
-// const COLOR_40_60 = '#fecc5c';
-// /** The color used to fill in growing units with a 60-80% closure probability. */
-// const COLOR_60_80 = '#fd8d3c';
-// /** The color used to fill in growing units with a 80-100% closure probability. */
-// const COLOR_80_100 = '#e31a1c';
+// /** The color used to fill in growing units with a Very Low risk. */
 const COLOR_VERY_LOW = '#4eb265';
+// /** The color used to fill in growing units with a Low risk. */
 const COLOR_LOW = '#fecc5c';
+// /** The color used to fill in growing units with a Moderate risk. */
 const COLOR_MODERATE = '#fd8d3c';
+// /** The color used to fill in growing units with a High risk. */
 const COLOR_HIGH = '#e31a1c';
+// /** The color used to fill in growing units with a Very High risk. */
 const COLOR_VERY_HIGH = '#8B0000';
 /** The text and colors used for the legend. */
 const LEGEND_SCALE = [
   { text: 'No data', color: COLOR_NULL},
-  // { text: '0 - 40%', color: COLOR_0_40},
-  // { text: '40 - 60%', color: COLOR_40_60},
-  // { text: '60 - 80%', color: COLOR_60_80},
-  // { text: '80 - 100%', color: COLOR_80_100}
   { text: 'Very Low', color: COLOR_VERY_LOW},
   { text: 'Low', color: COLOR_LOW},
   { text: 'Moderate', color: COLOR_MODERATE},
@@ -277,6 +270,10 @@ function getLatLngFromArray(geometry) {
   };
 }
 
+/**
+ * Returns a value of the risk factor.
+ * @param {number} value the value with probability
+ */
 function handleUndef(value) {
   let flag = ""
   if(value === 1)
@@ -316,16 +313,11 @@ function styleFeatureBasedOnDay(day) {
  */
 function getColor(value) {
   let color = 'transparent';
-  // if (value >= 0) color = COLOR_0_40;
-  // if (value >= 40) color = COLOR_40_60;
-  // if (value >= 60) color = COLOR_60_80;
-  // if (value >= 80) color = COLOR_80_100;
   if (value === 1) color = COLOR_VERY_LOW;
   if (value === 2) color = COLOR_LOW;
   if (value === 3) color = COLOR_MODERATE;
   if (value === 4) color = COLOR_HIGH;
   if (value === 5) color = COLOR_VERY_HIGH;
-  if (value === null) color = 'transparent';
   return color;
 }
 
