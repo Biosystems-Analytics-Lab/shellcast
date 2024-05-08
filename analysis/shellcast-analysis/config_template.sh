4838d02d7499602a60071b5936b2e59aca2b0f30
Makiko Shukunobe <mshukuno@users.noreply.github.com>
2024-05-07 20:32:48 -0400

    Add config_templage.sh

---
+++ analysis/shellcast-analysis/config_template.sh
@@ -0,0 +1,18 @@
+#! /bin/sh
+
+# Path to cloud_sql_proxy file
+CLOUD_SQL_PATH="{your directory}/shellcast/analysis/shellcast-analysis/cloud-sql-proxy"
+
+# Name of Cloud SQL Instance
+CLOUD_SQL_INSTANCE_NAME=""
+
+# cloud sql port number
+CLOUD_SQL_PORT="3306"
+
+
+PYTHON_PATH="{path to python}"
+VENV_ACTIVATE_PATH="{path to vertual environment activate}"
+
+FL_MAIN_PY="/{your directory}/shellcast/analysis/shellcast-analysis/fl_main.py"
+NC_MAIN_PY="/{your directory}/shellcast/Desktop/shellcast/analysis/shellcast-analysis/nc_main.py"
+SC_MAIN_PY="/{your directory}/shellcast/Desktop/shellcast/analysis/shellcast-analysis/sc_main.py"
