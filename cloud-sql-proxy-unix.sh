87fa52291c3b541b8831dc629832b4040624ef9e
mshukuno <mshukuno@users.noreply.github.com>
2024-06-20 18:14:46 -0400

    Update

--- cloud-sql-proxy-unix.sh
+++ cloud-sql-proxy-unix.sh
@@ -1,12 +1,7 @@
 #!/bin/sh
 
 # Unix socket connection - shellcast-web-{state}
+instance_connection_name="{Find the instance connection name in the Google Cloud Console}"
 
-# NC
-./cloud-sql-proxy --unix-socket "./web/shellcast-web-nc/cloudsql" "{instance_connection_name}"
-
-# SC
-./cloud-sql-proxy --unix-socket "./web/shellcast-web-sc/cloudsql" "{instance_connection_name}"
-
-# FL
-./cloud-sql-proxy --unix-socket "./web/shellcast-web-fl/cloudsql" "{instance_connection_name}"
+# Modify {state} to the state of the web application.
+./cloud-sql-proxy --unix-socket "./web/shellcast-web-{state}/cloudsql" ${instance_connection_name}
