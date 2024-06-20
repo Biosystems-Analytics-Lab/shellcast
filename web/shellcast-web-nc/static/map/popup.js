2f6651c92ae18e338507662fe070e21504860382
mshukuno <mshukuno@users.noreply.github.com>
2024-06-20 18:23:19 -0400

    Refactor map.js.

---
+++ web/shellcast-web-nc/static/map/popup.js
@@ -0,0 +1,21 @@
+import { handleUndef } from "./utils.js";
+const container = document.getElementById("popup");
+
+export function createShellCastPopupLayer() {
+    return new ol.Overlay({
+        element: container,
+        autoPan: {
+            animation: {
+                duration: 250,
+            },
+        },
+    });
+}
+
+export function generatePopUpContent(feature) {
+    return `<div>Growing Unit: ${feature.get("cmu_name")}
+                <br>Today: ${handleUndef(feature.get("prob_1d_perc"))}
+                <br>Tomorrow: ${handleUndef(feature.get("prob_2d_perc"))}
+                <br>In 2 days: ${handleUndef(feature.get("prob_3d_perc"))}
+                </div>`;
+}
---
+++ web/shellcast-web-nc/static/map/table.js
@@ -0,0 +1,51 @@
+import { handleUndef } from './utils.js';
+//** The ID of the growing unit table element. */
+const GROWING_UNIT_TABLE_ID = "growing-unit-table";
+/** The ID of the lease table element. */
+const LEASE_TABLE_ID = "lease-table";
+
+export function setTableSearchBoxes() {
+    // change placeholder text and title attribute for table search boxes
+    const leaseTableSearchBox = document.querySelector("#lease-table-div input");
+    leaseTableSearchBox.placeholder = "Search leases";
+    leaseTableSearchBox.title = "Search leases table";
+    const growingUnitTableSearchBox = document.querySelector(
+        "#growing-unit-table-div input",
+    );
+    growingUnitTableSearchBox.placeholder = "Search growing units";
+    growingUnitTableSearchBox.title = "Search growing units table";
+}
+/**
+ * Fills the growing unit table with the growing unit records.
+ */
+export function initGrowingUnitTable(growingUnitData) {
+  const rows = [];
+  for (let [cmuName, data] of Object.entries(growingUnitData)) {
+    const rowData = {
+      cmu_name: cmuName,
+      prob_1d_perc: `${handleUndef(data.prob_1d_perc)}`,
+      prob_2d_perc: `${handleUndef(data.prob_2d_perc)}`,
+      prob_3d_perc: `${handleUndef(data.prob_3d_perc)}`,
+    };
+    rows.push(rowData);
+  }
+  $(`#${GROWING_UNIT_TABLE_ID}`).bootstrapTable("load", rows);
+}
+
+/**
+ * Fills the lease table with lease records.
+ */
+export function initLeaseTable(leaseData) {
+  const rows = [];
+  for (let lease of leaseData) {
+    const rowData = {
+      lease_id: lease.lease_id,
+      prob_1d_perc: `${handleUndef(lease.prob_1d_perc)}`,
+      prob_2d_perc: `${handleUndef(lease.prob_2d_perc)}`,
+      prob_3d_perc: `${handleUndef(lease.prob_3d_perc)}`,
+    };
+    rows.push(rowData);
+  }
+  $(`#${LEASE_TABLE_ID}`).bootstrapTable("load", rows);
+}
+
