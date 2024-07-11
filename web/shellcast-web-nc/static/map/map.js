6b4e33f8181642b8088158ef9d2085543e29a650
mshukuno <mshukuno@users.noreply.github.com>
2024-07-11 12:54:44 -0400

    Adopt Firebase version 10.12.3 and remove Firebaseui.
    Firebaseui isn't compatible since v9 thus removed.

--- web/shellcast-web-nc/static/map/map.js
+++ web/shellcast-web-nc/static/map/map.js
@@ -1,4 +1,6 @@
 "use strict";
+import {auth, authorizedFetch} from "../common/common.js";
+import {onAuthStateChanged} from "https://www.gstatic.com/firebasejs/10.12.3/firebase-auth.js"; // Firebase configuration
 
 /** The ID of the HTML element that holds the map. */
 const MAP_EL_ID = "closure-map";
@@ -41,6 +43,8 @@ const LEGEND_SCALE = [
     color: COLOR_VERY_HIGH,
   },
 ];
+
+const DISCLAIMER_MODAL_ID = "disclaimer-privacy-modal";
 
 // a reference to the Google map object
 let map;
@@ -474,7 +478,7 @@ function addLeaseDataToMap(pointFeatures) {
   growingUnitTableSearchBox.title = "Search growing units table";
   initGrowingUnitTable(growingUnitData);
 
-  firebase.auth().onAuthStateChanged(async (user) => {
+  onAuthStateChanged(auth, async (user) => {
     if (user) {
       // hide disclaimer/privacy policy modal
       $(`#${DISCLAIMER_MODAL_ID}`).modal("hide");
@@ -488,10 +492,8 @@ function addLeaseDataToMap(pointFeatures) {
       document.getElementById("create-account-message").style.display = "none";
 
       const leaseData = await getLeaseData();
-      console.log(leaseData);
       initLeaseTable(leaseData);
       createLeasePointFeatures(leaseData).then((features) => {
-        console.log(features);
         addLeaseDataToMap(features);
       });
       // addLeaseDataToMap(leaseData);
