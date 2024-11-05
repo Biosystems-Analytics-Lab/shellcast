373fc3d649bc1ed8bcb42e868223841451e0cd2a
mshukuno <mshukuno@users.noreply.github.com>
2024-11-05 02:55:29 -0500

    Update `pqpf_procs.py`

--- analysis/shellcast-analysis/src/pqpf_procs.py
+++ analysis/shellcast-analysis/src/pqpf_procs.py
@@ -4,17 +4,21 @@ Steps:
 1. Listing today's PQPF file names for download.
 
 """
+
 import configparser
-import sys
-import os
 import logging.config
-import pygrib
-import warnings
+import os
 import platform
-import constants as ct
-from shapely.errors import ShapelyDeprecationWarning
+import sys
+import warnings
 from datetime import datetime
 from typing import List
+
+import pygrib
+import pytz
+from shapely.errors import ShapelyDeprecationWarning
+
+import constants as ct
 import utils
 
 warnings.filterwarnings("ignore", category=ShapelyDeprecationWarning)
@@ -31,25 +35,35 @@ class ProcDirs:
             state (str): State abbreviation
             db (str):
         """
-        self.os_type = 'Other' if platform.system() == 'Darwin' else 'Windows'
+        self.os_type = "Other" if platform.system() == "Darwin" else "Windows"
 
-        self.outfile_date = datetime.today().strftime('%Y-%m-%d')
+        self.date_today = datetime.now(pytz.timezone("America/New_York")).today()
         self.config = configparser.ConfigParser()
         self.config.read(ct.CONFIG_INI)
         self.state = state.upper()
         self.data_root = os.path.join(ct.PQPF_DATA_DIR, state.lower())
-        self.connect_str = utils.get_connection_string(self.config[db], self.config[self.state]['DB_NAME'])
+        self.connect_str = utils.get_connection_string(
+            self.config[db], self.config[self.state]["DB_NAME"]
+        )
         # Data directories
-        self.inputs_dir = os.path.join(self.data_root, 'inputs')
-        self.outputs_dir = utils.create_directory(os.path.join(self.data_root, 'outputs'), delete=True)
-        self.grb_raw_dir = utils.create_directory(os.path.join(ct.PQPF_DATA_DIR, 'raw'))
-        self.intermediate_dir = utils.create_directory(os.path.join(self.data_root, 'intermediate'),
-                                                               delete=True)
-        self.grb_subsets_dir = utils.create_directory(os.path.join(self.intermediate_dir, 'subsets'),
-                                                      delete=True)
-        self.tiffs_dir = utils.create_directory(os.path.join(self.intermediate_dir, 'tiffs'), delete=True)
+        self.inputs_dir = os.path.join(self.data_root, "inputs")
+        self.outputs_dir = utils.create_directory(
+            os.path.join(self.data_root, "outputs"), delete=True
+        )
+        self.grb_raw_dir = utils.create_directory(os.path.join(ct.PQPF_DATA_DIR, "raw"))
+        self.intermediate_dir = utils.create_directory(
+            os.path.join(self.data_root, "intermediate"), delete=True
+        )
+        self.grb_subsets_dir = utils.create_directory(
+            os.path.join(self.intermediate_dir, "subsets"), delete=True
+        )
+        self.tiffs_dir = utils.create_directory(
+            os.path.join(self.intermediate_dir, "tiffs"), delete=True
+        )
 
-        self.lease_shp = os.path.join(self.inputs_dir, self.config[self.state]['LEASE_SHP'])
+        self.lease_shp = os.path.join(
+            self.inputs_dir, self.config[self.state]["LEASE_SHP"]
+        )
 
 
 class PQPFProcs:
@@ -69,14 +83,14 @@ class PQPFProcs:
         Returns:
             object: list of today's PQPF GRB files
         """
-        logger.info('[PQPF GRB files to download]')
+        logger.info("[PQPF GRB files to download]")
         try:
             files = []
             files_to_download = []
-            today = datetime.today().strftime('%Y%m%d')
+            today = datetime.today().strftime("%Y%m%d")
             # Get GRB names for today
             for hour in ct.VALID_HOURS:
-                fname = f'{ct.GRB_PREFIX}_{today}{ct.Z_RUN}{hour}.grb'
+                fname = f"{ct.GRB_PREFIX}_{today}{ct.Z_RUN}{hour}.grb"
                 files.append(fname)
             # Check today's GRB files are already in directory
             if len(files) > 0:
@@ -88,15 +102,15 @@ class PQPFProcs:
                         logger.info(f)
                     return files_to_download
                 else:
-                    logger.info('Data exists in the directory.')
+                    logger.info("Data exists in the directory.")
                 logger.info(utils.done_str)
             else:
-                msg = ' Having a problem listing PQPF GRB files.'
+                msg = " Having a problem listing PQPF GRB files."
                 logger.error(msg)
                 utils.send_email(msg, self.state)
                 sys.exit(1)
         except Exception as e:
-            msg = 'Files to download failed.'
+            msg = "Files to download failed."
             utils.error_process(msg, e)
 
     def check_grb_files(self):
@@ -105,59 +119,65 @@ class PQPFProcs:
         TODO - Make analysis done with downloaded files. For instance, one day, there is no f030 PQPF data then run analysis with rest of PQPF files.
 
         """
-        logger.info('[Check GRB files dates]')
+        logger.info("[Check GRB files dates]")
         grb_file_dates = []
         to_db_bool = False
         grb_files = os.listdir(self.grb_raw_dir)
         today_dt = datetime.today()
-        today_str = datetime.strftime(today_dt, '%Y-%m-%d')
+        today_str = datetime.strftime(today_dt, "%Y-%m-%d")
         for f in grb_files:
-            if f.endswith('.grb'):
-                date = f.split('_')[-1][:8]
+            if f.endswith(".grb"):
+                date = f.split("_")[-1][:8]
                 grb_file_dates.append(date)
         unique_dates = list(set(grb_file_dates))
         if len(unique_dates) == 1:
-            dt = datetime.strptime(unique_dates[0], '%Y%m%d')
-            self.outfile_date = datetime.strftime(dt, '%Y-%m-%d')
+            dt = datetime.strptime(unique_dates[0], "%Y%m%d")
+            self.outfile_date = datetime.strftime(dt, "%Y-%m-%d")
             if self.outfile_date == today_str:
                 to_db_bool = True
-            logger.info(f'Date of data: {self.outfile_date}')
+            logger.info(f"Date of data: {self.outfile_date}")
             logger.info(utils.done_str)
             return to_db_bool
         else:
-            msg = 'All GRB files need to have the same date.'
+            msg = "All GRB files need to have the same date."
             logger.error(msg)
             sys.exit(0)
 
     def wgrib2_small_grib(self, grb_fpath):
         grb_fname = os.path.basename(grb_fpath)
         try:
-            wgrib2 = 'wgrib2'
-            if self.os_type == 'Other':
-                wgrib2 = '/usr/local/bin/wgrib2'
+            wgrib2 = "wgrib2"
+            if self.os_type == "Other":
+                wgrib2 = "/usr/local/bin/wgrib2"
 
-            out_grb_path = os.path.join(self.grb_subsets_dir, f'sbs_{grb_fname}')
-            cmd = [wgrib2, grb_fpath, '-small_grib', self.config[self.state]['LON_WE'],
-                   self.config[self.state]['LAT_SN'], out_grb_path]
+            out_grb_path = os.path.join(self.grb_subsets_dir, f"sbs_{grb_fname}")
+            cmd = [
+                wgrib2,
+                grb_fpath,
+                "-small_grib",
+                self.config[self.state]["LON_WE"],
+                self.config[self.state]["LAT_SN"],
+                out_grb_path,
+            ]
             utils.cmd_subprocess(cmd)
-            logger.info(f'{grb_fname} --- cropped')
+            logger.info(f"{grb_fname} --- cropped")
 
         except Exception as e:
-            msg = f'{grb_fname} --- subset GRIB file failed.'
+            msg = f"{grb_fname} --- subset GRIB file failed."
             utils.error_process(msg, e)
 
     def small_grb(self) -> None:
         """
         Crop GRB files in small area.
         """
-        logger.info('[Subset GRB file for small area]')
+        logger.info("[Subset GRB file for small area]")
         try:
             utils.delete_files(self.grb_subsets_dir)
             grbs = utils.get_raw_grb_list(self.grb_raw_dir)
             if grbs and len(grbs) > 0:
-                if self.state == 'FL':
+                if self.state == "FL":
                     for grb in grbs:
-                        if grb.endswith('f030.grb'):
+                        if grb.endswith("f030.grb"):
                             self.wgrib2_small_grib(grb)
                 else:
                     for grb in grbs:
@@ -165,7 +185,7 @@ class PQPFProcs:
             logger.info(utils.done_str)
 
         except Exception as e:
-            msg = 'Subset GRIB file failed.'
+            msg = "Subset GRIB file failed."
             utils.error_process(msg, e)
 
     def grb_to_tiff(self, thresholds) -> None:
@@ -174,31 +194,39 @@ class PQPFProcs:
         Args:
             thresholds (List[float]): List of unique rainfall thresholds
         """
-        logger.info('[Transform GRB to Tiff for each threshold]')
+        logger.info("[Transform GRB to Tiff for each threshold]")
         try:
             utils.delete_files(self.tiffs_dir)
-            dst_srs = 'EPSG:4326' if self.state == 'FL' else None
+            dst_srs = "EPSG:4326" if self.state == "FL" else None
 
-            for grb_fpath in [os.path.join(self.grb_subsets_dir, f) for f in os.listdir(self.grb_subsets_dir) if
-                              f.endswith('grb')]:
+            for grb_fpath in [
+                os.path.join(self.grb_subsets_dir, f)
+                for f in os.listdir(self.grb_subsets_dir)
+                if f.endswith("grb")
+            ]:
                 grbs = pygrib.open(grb_fpath)
-                fname = grb_fpath.split('_')[-1].split('.')[0]
+                fname = grb_fpath.split("_")[-1].split(".")[0]
 
                 for idx, grb in enumerate(grbs):
-                    if 'upperLimit' in grb.keys():
+                    if "upperLimit" in grb.keys():
                         # upper_limit = grb.upperLimit
                         # inches = round((upper_limit / 1000) / 25.4, 1)
                         inches = round(grb.upperLimit / 25.4, 1)
                         for threshold in thresholds:
                             if float(threshold) == inches:
-                                rainfall_str = str(threshold).replace('.', 'p')
-                                tiff_path = os.path.join(self.tiffs_dir, f'{ct.TIFF_PREFIX}_{fname}_{rainfall_str}.tif')
-                                utils.run_gdal_to_tiff(grb_fpath, tiff_path, {idx + 1}, dst_srs)
+                                rainfall_str = str(threshold).replace(".", "p")
+                                tiff_path = os.path.join(
+                                    self.tiffs_dir,
+                                    f"{ct.TIFF_PREFIX}_{fname}_{rainfall_str}.tif",
+                                )
+                                utils.run_gdal_to_tiff(
+                                    grb_fpath, tiff_path, {idx + 1}, dst_srs
+                                )
                                 # cmd = [gdal_translate, '-b', f'{idx + 1}', '-of', 'GTiff', grb_fpath, tiff_path]
                                 # utils.cmd_subprocess(cmd)
             logger.info(utils.done_str)
         except Exception as e:
-            msg = 'GRB to TIFF transformation failed.'
+            msg = "GRB to TIFF transformation failed."
             utils.error_process(msg, e)
 
     def grb_to_tiff_all(self) -> None:
@@ -207,24 +235,30 @@ class PQPFProcs:
         Args:
             thresholds (List[float]): List of unique rainfall thresholds
         """
-        logger.info('[Transform all GRB thresholds to TIFF]')
+        logger.info("[Transform all GRB thresholds to TIFF]")
         try:
             utils.delete_files(self.tiffs_dir)
-            dst_srs = 'EPSG:4326' if self.state == 'FL' else None
-            for grb_fpath in [os.path.join(self.grb_subsets_dir, f) for f in os.listdir(self.grb_subsets_dir) if
-                              f.endswith('grb')]:
+            dst_srs = "EPSG:4326" if self.state == "FL" else None
+            for grb_fpath in [
+                os.path.join(self.grb_subsets_dir, f)
+                for f in os.listdir(self.grb_subsets_dir)
+                if f.endswith("grb")
+            ]:
                 grbs = pygrib.open(grb_fpath)
-                fname = grb_fpath.split('_')[-1].split('.')[0]
+                fname = grb_fpath.split("_")[-1].split(".")[0]
 
                 for idx, grb in enumerate(grbs):
-                    if 'upperLimit' in grb.keys():
+                    if "upperLimit" in grb.keys():
                         # upper_limit = grb.upperLimit
                         # inches = round((upper_limit / 1000) / 25.4, 1)
                         inches = float(round(grb.upperLimit / 25.4, 1))
-                        rainfall_str = str(inches).replace('.', 'p')
-                        tiff_path = os.path.join(self.tiffs_dir, f'{ct.TIFF_PREFIX}_{fname}_{rainfall_str}.tif')
+                        rainfall_str = str(inches).replace(".", "p")
+                        tiff_path = os.path.join(
+                            self.tiffs_dir,
+                            f"{ct.TIFF_PREFIX}_{fname}_{rainfall_str}.tif",
+                        )
                         utils.run_gdal_to_tiff(grb_fpath, tiff_path, {idx + 1}, dst_srs)
             logger.info(utils.done_str)
         except Exception as e:
-            msg = 'GRB to TIFF transformation failed.'
+            msg = "GRB to TIFF transformation failed."
             utils.error_process(msg, e)
