16f4613557c0f3b8905e751191667c646d9af523
mshukuno <mshukuno@users.noreply.github.com>
2024-11-06 05:37:55 -0500

    Update Lease model

--- web/shellcast-web-fl/models/Lease.py
+++ web/shellcast-web-fl/models/Lease.py
@@ -24,15 +24,18 @@ class Lease(db.Model):
         return {
             'lease_id': self.lease_id,
             'cmu_id': self.cmu_id,
+            'sh_id': self.cmus.sh_id,
+            'sh_name': self.cmus.sh_name,
             'parcel_name': self.parcel_name,
             'waterbody': self.waterbody,
             'grow_area_type': self.grow_area_type,
+            'rainfall_desc': self.cmus.rainfall_desc,
             'latitude': self.latitude,
             'longitude': self.longitude
         }
 
     def __repr__(self):
         return ('<Lease: '
-                f'{self.lease_id}, {self.cmu_id}, '
+                f'{self.lease_id}, {self.cmu_id}, {self.sh_id},'
                 f'{self.parcel_name}, {self.waterbody}, {self.grow_area_type}, '
                 f'{self.latitude}, {self.latitude}>')
