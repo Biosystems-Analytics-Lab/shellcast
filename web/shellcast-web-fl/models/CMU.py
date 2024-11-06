b865800515cc7efd6e564e5ed70d7eb9e7474c72
mshukuno <mshukuno@users.noreply.github.com>
2024-11-06 02:04:56 -0500

    Update `CMU` models to include `season`

--- web/shellcast-web-fl/models/CMU.py
+++ web/shellcast-web-fl/models/CMU.py
@@ -1,37 +1,37 @@
-import json
-from sqlalchemy import Column, Integer, String, DateTime, Float
-from sqlalchemy.sql import functions
-from sqlalchemy.orm import relationship
-
 from models import db
+from sqlalchemy import Column, Integer, String, DateTime, Float
+from sqlalchemy.orm import relationship
+from sqlalchemy.sql import functions
+
 
 class CMU(db.Model):
-  __tablename__ = 'cmus'
+    __tablename__ = 'cmus'
 
-  id = Column(String(10), primary_key=True)
-  sh_name = Column(String(50), nullable=False)
-  rainfall_desc = Column(String(100))
-  rainfall_thresh_days = Column(Integer)
-  rainfall_thresh_in = Column(Float)
-  emerg_cond = Column(String(3))
-  created = Column(DateTime, server_default=functions.now())
+    id = Column(String(10), primary_key=True)
+    sh_id = Column(String(10), nullable=False)
+    sh_name = Column(String(50))
+    rainfall_desc = Column(String(100))
+    rainfall_thresh_days = Column(Integer, nullable=False)
+    rainfall_thresh_in = Column(Float, nullable=False)
+    season = Column(String(100))
+    created = Column(DateTime, server_default=functions.now())
 
-  cmu_probabilities = relationship('CMUProbability', back_populates='cmus')
-  leases = relationship('Lease', back_populates='cmus')
-  # updated = Column(DateTime, server_default=functions.now(), onupdate=functions.now())
-  def asDict(self):
-    return {
-      'id': self.id,
-      'sh_name': self.sh_name,
-      'rainfall_desc': self.rainfall_desc.replace('\"', '"'),
-      'rainfall_thresh_days': self.rainfall_thresh_days,
-      'rainfall_thresh_in': self.rainfall_thresh_in,
-      'emerg_cond': self.emerg_cond
-    }
+    cmu_probabilities = relationship('CMUProbability', back_populates='cmus')
+    leases = relationship('Lease', back_populates='cmus')
 
-  def __repr__(self):
-      return ('<CMU: '
-              f'{self.id}, {self.sh_name}, {self.rainfall_desc}, '
-              f'{self.rainfall_thresh_days}, {self.rainfall_thresh_in}, '
-              f'{self.emerg_cond}>')
+    # updated = Column(DateTime, server_default=functions.now(), onupdate=functions.now())
+    def asDict(self):
+        return {
+            'id': self.id,
+            'sh_id': self.sh_id,
+            'sh_name': self.sh_name,
+            'rainfall_desc': self.rainfall_desc.replace('\"', '"'),
+            'rainfall_thresh_days': self.rainfall_thresh_days,
+            'rainfall_thresh_in': self.rainfall_thresh_in,
+            'season': self.season,
+        }
 
+    def __repr__(self):
+        return ('<CMU: '
+                f'{self.id}, {self.sh_id}, {self.sh_name}, {self.rainfall_desc}, '
+                f'{self.rainfall_thresh_days}, {self.rainfall_thresh_in}, {self.season}>')
