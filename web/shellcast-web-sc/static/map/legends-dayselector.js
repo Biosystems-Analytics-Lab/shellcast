df21fe7d20c02d6e9bd107d04ec5b897766018d0
mshukuno <mshukuno@users.noreply.github.com>
2025-03-06 18:56:02 -0500

    format: User JS formatter

---
+++ web/shellcast-web-sc/static/map/legends-dayselector.js
@@ -0,0 +1,137 @@
+"use strict";
+
+import { strToEl } from "./utils.js";
+import {
+  CAMERA_ICON,
+  HOWS_THE_BEACH_ICON,
+  LEGEND_SCALE,
+  VISIT_BEACHES_ICON,
+} from "./map_constants.js";
+
+/**
+ * Create CMU closure probability legend on map
+ * @returns {HTMLDivElement}
+ */
+export function addShellCastLegendControl(legendID) {
+  const legend = document.createElement("div");
+  legend.id = legendID;
+  for (let step of LEGEND_SCALE) {
+    const textDiv = strToEl(
+      `<div id="shellcast-legend-txt">${step.text}</div>`
+    );
+    legend.appendChild(textDiv);
+    const colorDiv = document.createElement("div");
+    colorDiv.id = "shellcast-legend-color";
+    colorDiv.style.backgroundColor = step.color;
+    legend.appendChild(colorDiv);
+  }
+  return new ol.control.Control({ element: legend });
+}
+
+// export function addShellCastLegend(legendID) {
+//   const shellCastLegend = new ShellCastLegend();
+//   const legendLeft = shellCastLegend.create(legendID);
+//   return new ol.control.Control({ element: legendLeft });
+// }
+
+export function addPartnerSitesLegendControl(partnerSitesLegendID) {
+  const partnerSitesLegend = document.createElement("div");
+  partnerSitesLegend.id = partnerSitesLegendID;
+  partnerSitesLegend.innerHTML = `
+    <div id="partner-sites-legend">
+      <div id="accordion" style="width: 20rem">
+        <div class="card">
+          <div class="card-header text-center" id="headingOne" style="padding: .1rem;">
+            <h5 class="mb-0">
+              <button class="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true"
+                aria-controls="collapseOne">
+                Legend
+              </button>
+            </h5>
+          </div>
+          <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
+            <div class="card-body">
+              <h6>ShellCast</h6>
+              <small>The legend for ShellCast is shown to the left. The forecast date can be changed by clicking 
+              "Today", "Tomorrow" and "In 2 days".</small>
+              <div style="line-height: 20px; vertical-align: middle; padding-top: 16px;">
+                <input type="checkbox" id="partner-sites-legend-checkbox" role="button"
+                  style="width: 20px;height: 20px; vertical-align: middle;" checked>
+                <label class="form-check-label" for="partner-sites-legend-checkbox"><span>
+                    <h6>&emsp13;Partner Sites</h6>
+                  </span></label>
+              </div>      
+              <table id="partner-sites-legend-table" class="partner-legend">
+                <tr>
+                  <td class="legend-icon"><img src=${HOWS_THE_BEACH_ICON} alt="How's the Beach"></td>
+                  <td><small>How's the Beach Sites</small></td>
+                </tr>
+                <tr>
+                  <td class="legend-icon"><img src=${VISIT_BEACHES_ICON} alt="Visit Beachs"></td>
+                  <td><small>Beach Conditions Reporting System</small></td>
+                </tr>
+                <tr>
+                  <td class="legend-icon"><img src=${CAMERA_ICON} alt="WebCOOS"</td>
+                  <td><small>WebCOOS Camera Sites</small></td>
+                </tr>
+            </table>      
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>`;
+  return new ol.control.Control({ element: partnerSitesLegend });
+}
+
+// function addPartnerSitesLegend() {
+//   const partnerSitesLegend = document.createElement("div");
+//   partnerSitesLegend.id = legendID;
+//   return new ol.control.Control({
+//     element: partnerAppLyrLegend,
+//   });
+// }
+
+// export let partnerAppLyrLegend = document.createElement("div");
+// partnerAppLyrLegend.innerHTML = `
+// <div id="partner-sites-legend">
+//   <div id="accordion" style="width: 20rem">
+//     <div class="card">
+//       <div class="card-header text-center" id="headingOne" style="padding: .1rem;">
+//         <h5 class="mb-0">
+//           <button class="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true"
+//             aria-controls="collapseOne">
+//             Legend
+//           </button>
+//         </h5>
+//       </div>
+//       <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
+//         <div class="card-body">
+//           <h6>ShellCast</h6>
+//           <small>The legend for ShellCast is shown to the left. The forecast date can be changed by clicking
+//           "Today", "Tomorrow" and "In 2 days".</small>
+//           <div style="line-height: 20px; vertical-align: middle; padding-top: 16px;">
+//             <input type="checkbox" id="partner-legend" role="button"
+//               style="width: 20px;height: 20px; vertical-align: middle;" checked>
+//             <label class="form-check-label" for="partner-legend"><span>
+//                 <h6>&emsp13;Partner Sites</h6>
+//               </span></label>
+//           </div>
+//           <table id="legend-table" class="partner-legend">
+//             <tr>
+//               <td class="legend-icon"><img src=${HOWS_THE_BEACH_ICON} alt="How's the Beach"></td>
+//               <td><small>How's the Beach Sites</small></td>
+//             </tr>
+//             <tr>
+//               <td class="legend-icon"><img src=${VISIT_BEACHES_ICON} alt="Visit Beachs"></td>
+//               <td><small>Beach Conditions Reporting System</small></td>
+//             </tr>
+//             <tr>
+//               <td class="legend-icon"><img src=${CAMERA_ICON} alt="WebCOOS"</td>
+//               <td><small>WebCOOS Camera Sites</small></td>
+//             </tr>
+//         </table>
+//         </div>
+//       </div>
+//     </div>
+//   </div>
+// </div>`;
---
+++ web/shellcast-web-sc/static/map/utils.js
@@ -0,0 +1,270 @@
+"use strict";
+import { authorizedFetch } from "../common/common.js";
+import {
+  COLOR_PALLET,
+  GROWING_UNIT_BOUNDS_PATH,
+  HOME_ICON,
+  PARTNER_APP_POINTS_PATH,
+} from "./map_constants.js";
+
+/**
+ * Returns a value of the risk factor.
+ * @param {number} value the value with probability
+ */
+export function handleUndef(value) {
+  let flag = "";
+  if (value === 1) flag = "Very Low";
+  if (value === 2) flag = "Low";
+  if (value === 3) flag = "Moderate";
+  if (value === 4) flag = "High";
+  if (value === 5) flag = "Very High";
+  return value || value === 0 ? flag : "-";
+}
+
+export function getDomainName(url) {
+  try {
+    const urlObj = new URL(url);
+    return urlObj.hostname;
+  } catch (e) {
+    console.error("Invalid URL", e);
+    return null;
+  }
+}
+
+/**
+ * @param {string} htmlStr the HTML string to create an element from
+ * @return {Element} a DOM element created from the given string
+ */
+export let strToEl = function (htmlStr) {
+  const template = document.createElement("template");
+  template.innerHTML = htmlStr.trim();
+  return template.content.firstChild;
+};
+
+// ================== API requests ==================
+/**
+ * Fetches the growing unit data from the server and returns it.
+ */
+export async function getGrowingUnitData() {
+  const res = await fetch("/growingUnitProbs");
+  if (res.ok) {
+    return await res.json();
+  }
+  console.log("Problem retrieving growing unit data.");
+  console.log(res);
+  return null;
+}
+
+/**
+ * Fetches the current user's lease data from the server and returns it.
+ */
+export async function getLeaseData() {
+  const res = await authorizedFetch("/leaseProbs");
+  if (res.ok) {
+    return await res.json();
+  }
+  console.log("Problem retrieving lease data.");
+  console.log(res);
+  return null;
+}
+
+// ================== Get GeoJSON ==================
+/**
+ * Add GeoJson CMU polygon feature to closure probabilities properties.
+ * @param growingUnitData
+ * @returns {Promise<any>}
+ */
+export async function getGeoJsonAddProbs(growingUnitData) {
+  return fetch(GROWING_UNIT_BOUNDS_PATH)
+    .then((response) => response.json())
+    .then((response) => {
+      response.features.forEach((feature) => {
+        for (let [lease_id, data] of Object.entries(growingUnitData)) {
+          if (feature.properties.lease_id === lease_id) {
+            feature.properties.prob_1d_perc = data.prob_1d_perc;
+            feature.properties.prob_2d_perc = data.prob_2d_perc;
+            feature.properties.prob_3d_perc = data.prob_3d_perc;
+          }
+        }
+      });
+      return response;
+    })
+    .then((res) => {
+      return res;
+    });
+}
+
+export function createCmuGeoJsonSource(cmuGeoJson) {
+  return new ol.source.Vector({
+    features: new ol.format.GeoJSON().readFeatures(cmuGeoJson, {
+      dataProjection: "EPSG:4326",
+      featureProjection: "EPSG:3857",
+    }),
+  });
+}
+
+export async function getPartnerSitesSourceData() {
+  return new ol.source.Vector({
+    url: PARTNER_APP_POINTS_PATH,
+    format: new ol.format.GeoJSON(),
+  });
+}
+
+export async function createClusterSource(sourceData) {
+  return new ol.source.Cluster({
+    distance: 50,
+    source: sourceData,
+  });
+}
+
+export function createBaseLayer() {
+  return new ol.layer.Tile({
+    source: new ol.source.OSM({
+      source: "http://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png",
+    }),
+  });
+}
+
+export function createCmuLayer(cmuGeoJsonSource, htmlElement) {
+  return new ol.layer.Vector({
+    name: htmlElement,
+    opacity: 0.7,
+    source: cmuGeoJsonSource,
+    extent: cmuGeoJsonSource.getExtent(),
+    style: function (feature) {
+      return getBoundaryStyle(feature, 1);
+    },
+  });
+}
+
+export function createSimplePopupLayer(containerEle) {
+  return new ol.Overlay({
+    element: containerEle,
+    autoPan: {
+      animation: {
+        duration: 250,
+      },
+    },
+  });
+}
+
+// ================== Styling ==================
+export function getColor(value) {
+  let color = COLOR_PALLET.COLOR_NULL;
+  if (value === 1) color = COLOR_PALLET.COLOR_VERY_LOW;
+  if (value === 2) color = COLOR_PALLET.COLOR_LOW;
+  if (value === 3) color = COLOR_PALLET.COLOR_MODERATE;
+  if (value === 4) color = COLOR_PALLET.COLOR_HIGH;
+  if (value === 5) color = COLOR_PALLET.COLOR_VERY_HIGH;
+  return color;
+}
+
+function fillBoundaryColor(colorValue, featureName) {
+  return new ol.style.Style({
+    fill: new ol.style.Fill({
+      color: getColor(colorValue),
+    }),
+    stroke: new ol.style.Stroke({
+      color: "rgba(255, 255, 255, 0.5)",
+      width: 1,
+    }),
+    text: new ol.style.Text({
+      text: featureName,
+      font: "Arial",
+      size: "12px",
+      fill: new ol.style.Fill({
+        color: "black",
+      }),
+      stroke: new ol.style.Stroke({
+        color: "white",
+        width: 3,
+      }),
+    }),
+    textBaseline: "middle",
+  });
+}
+
+export function getBoundaryStyle(feature, day) {
+  const value = feature.get(`prob_${day}d_perc`);
+  const featureName = feature.get("lease_id");
+  return fillBoundaryColor(value, featureName);
+}
+
+/**
+ * Create closure day button selector on map.
+ * @returns {Element}
+ */
+export function createDaySelector() {
+  const htmlStr = `
+    <div class="btn-group btn-group-toggle btn-group-vertical day-selector" data-toggle="buttons">
+      <label class="btn btn-outline-secondary active">
+        <input type="radio" id="1day" checked> Today
+      </label>
+      <label class="btn btn-outline-secondary">
+        <input type="radio" id="2day"> Tomorrow
+      </label>
+      <label class="btn btn-outline-secondary">
+        <input type="radio" id="3day"> In 2 days
+      </label>
+    </div>
+  `;
+  const daySelector = strToEl(htmlStr);
+  daySelector.style.backgroundColor = "white";
+  daySelector.style.margin = "10px";
+  daySelector.style.border = "1px solid black";
+  return daySelector;
+}
+
+function createHomeExtentButton() {
+  const homeExtentButton = document.createElement("div");
+  homeExtentButton.innerHTML = `<button id='home-ext-btn'><img id='home-ext-icon' src='${HOME_ICON}' alt='Home'/></button>`;
+  homeExtentButton.style.marginTop = "60px";
+  return homeExtentButton;
+}
+
+export function addHomeExtentButton(map, extent, popupLyr) {
+  const homeExtentButton = createHomeExtentButton();
+  homeExtentButton.onclick = () => {
+    map.getView().fit(extent, {
+      duration: 1000,
+      padding: [50, 50, 50, 50],
+    });
+    popupLyr.setPosition(undefined);
+  };
+  const homeExtent = new ol.control.Control({
+    element: homeExtentButton,
+  });
+  map.addControl(homeExtent);
+}
+
+// This sets the initial map extent as a maximum extent.
+// User can't pan outside of this extent.
+export function mapBoundingBox(map) {
+  map.setView(
+    new ol.View({
+      center: map.getView().getCenter(),
+      extent: map.getView().calculateExtent(map.getSize()),
+      zoom: map.getView().getZoom(),
+      padding: [50, 50, 50, 50],
+    })
+  );
+  return map.getView().calculateExtent(map.getSize());
+}
+
+export function addAllMapLayers(
+  mapEl,
+  lyrList,
+  popupLyr,
+  mapCenter,
+  zoomLevel
+) {
+  return new ol.Map({
+    target: mapEl,
+    layers: lyrList,
+    overlays: [popupLyr],
+    view: new ol.View({
+      center: ol.proj.fromLonLat(mapCenter),
+      zoom: zoomLevel,
+    }),
+  });
+}
