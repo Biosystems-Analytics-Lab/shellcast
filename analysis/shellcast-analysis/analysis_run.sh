8cde3da421405106d38c703fa423fa3bd083f501
mshukuno <mshukuno@users.noreply.github.com>
2025-05-07 16:06:16 -0400

    but: Fix the 'config.sh' directory

--- analysis/shellcast-analysis/analysis_run.sh
+++ analysis/shellcast-analysis/analysis_run.sh
@@ -1,19 +1,20 @@
 #! /bin/sh
 
+
 # source config.sh file (in analysis directory)
-source ./config.sh
+source $PWD"/config.sh"
 
 # make sure that all spawned processes are killed on exit, a kill signal, or an error
 trap "exit" INT TERM ERR
 trap "kill 0" EXIT
 
 # open TCP connection for step 4
-${CLOUD_SQL_PATH} --port 3306 ${CLOUD_SQL_INSTANCE_NAME} & PID1=$!
+#${CLOUD_SQL_PATH} --port 3306 ${CLOUD_SQL_INSTANCE_NAME} & PID1=$!
 
 source ${VENV_ACTIVATE_PATH}
 
-python $NC_MAIN_PY
-python $SC_MAIN_PY
+#python $NC_MAIN_PY
+#python $SC_MAIN_PY
 python $FL_MAIN_PY
 
 deactivate
