6e9f93e3d3570079f838bea583a0e3d7142c84a2
mshukuno <mshukuno@users.noreply.github.com>
2025-05-12 14:56:31 -0400

    bug: Separate user and developer notification switch

--- analysis/shellcast-analysis/fl_main.py
+++ analysis/shellcast-analysis/fl_main.py
@@ -6,8 +6,8 @@ Date: November 2023-2024
 import sys
 from pathlib import Path
 
-shellcast_analysis_dir = str(Path(__file__).resolve().parent)
-script_dir = str(Path(Path(shellcast_analysis_dir), "src"))
+shellcast_analysis_dir = str(Path().absolute().parents[1])
+script_dir = str(Path(Path().absolute(), "src"))
 sys.path.append(script_dir)
 
 import setup_logging  # noqa: E402
@@ -20,7 +20,7 @@ setup_logging.setup_logger(STATE)
 from fl_pqpf.fl_pqpf import FLPQPF  # noqa: E402
 from fl_pqpf.tp_xmrg import TPXMRG  # noqa: E402
 from management import DirectoryConfig, NotificationConfig  # noqa: E402
-from notifications import EmailNotification, DevEmailNotificationFL  # noqa: E402
+from notifications import EmailNotification, DevEmailNotificationFL # noqa: E402
 import logging  # noqa: E402
 
 logger = logging.getLogger(__name__)
@@ -32,19 +32,21 @@ if __name__ == "__main__":
     # DB connection information in config.ini
     db = "gcp.mysql"
 
-    # --- Download and process total precipitation XMRG dataset ---
-    tpxmrg = TPXMRG(STATE, 7)
-    tpxmrg.main()
+    # # --- Download and process total precipitation XMRG dataset ---
+    # tpxmrg = TPXMRG(STATE, 7)
+    # tpxmrg.main()
 
     # --- Directory configurations ---
     dir_config = DirectoryConfig(STATE, db)
 
-    # --- PQPF analysis ---
-    pqpf = FLPQPF(dir_config, save=True)
-    pqpf.main()
+    # # --- PQPF analysis ---
+    # pqpf = FLPQPF(dir_config)
+    # pqpf.main()
 
     # --- Email notification ---
     notification_config = NotificationConfig(STATE)
+    
+    # --- User Email Notification ---
     if notification_config.notifications_enabled:
         try:
             email_notify_inst = EmailNotification(
@@ -53,15 +55,19 @@ if __name__ == "__main__":
             email_notify_inst.send()
         except Exception as e:
             logger.error(f"Failed to send email notification: {str(e)}")
+    else:
+        logger.info(f"User notifications are disabled for {STATE} in configuration")
 
-        # --- Developer Email ---
+    # --- Developer Email ---
+    if notification_config.dev_send_email:
         try:
             dev_email_notify_inst = DevEmailNotificationFL(dir_config, notification_config)
             dev_email_notify_inst.send()
+
         except Exception as e:
             logger.error(f"Failed to send developer email notification: {str(e)}")
     else:
-        logger.info(f"Notifications are disabled for {STATE} in configuration")
+        logger.info(f"Developer notifications are disabled for {STATE} in configuration")
 
     # ---------------------
     logger.info(f"{'=' * 50}")
