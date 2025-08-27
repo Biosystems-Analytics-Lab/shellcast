c699ea59129f58abcb89ac0c1bcd5f9afece3353
mshukun_ncstate <mshukuno@users.noreply.github.com>
2025-08-27 16:53:42 -0400

    models: Update User model with email unsubscription fields

--- web/shellcast-web-sc/models/User.py
+++ web/shellcast-web-sc/models/User.py
@@ -13,6 +13,8 @@ class User(db.Model):
     DEFAULT_email_pref = False
     DEFAULT_text_pref = False
     DEFAULT_prob_pref = 3
+    DEFAULT_email_consent = False
+    DEFAULT_text_consent = False
 
     id = Column(Integer, primary_key=True)
     firebase_uid = Column(String(28))
@@ -24,10 +26,22 @@ class User(db.Model):
         default=DEFAULT_email_pref,
         nullable=False,
     )
+    email_consent = Column(
+        Boolean,
+        server_default=expression.false(),
+        default=DEFAULT_email_consent,
+        nullable=False,
+    )
     text_pref = Column(
         Boolean,
         server_default=expression.false(),
         default=DEFAULT_text_pref,
+        nullable=False,
+    )
+    text_consent = Column(
+        Boolean,
+        server_default=expression.false(),
+        default=DEFAULT_text_consent,
         nullable=False,
     )
     prob_pref = Column(
@@ -36,6 +50,12 @@ class User(db.Model):
         default=DEFAULT_prob_pref,
         nullable=False,
     )
+    email_opt_in_date = Column(DateTime)
+    text_opt_in_date = Column(DateTime)
+    email_opt_out_date = Column(DateTime)
+    text_opt_out_date = Column(DateTime)
+    email_verification_sent = Column(Boolean, server_default=expression.false(), default=False)
+    text_verification_sent = Column(Boolean, server_default=expression.false(), default=False)
     deleted = Column(Boolean, server_default=expression.false(), default=False)
     created = Column(DateTime, server_default=functions.now())
     updated = Column(DateTime, server_default=functions.now(), onupdate=functions.now())
@@ -53,8 +73,12 @@ class User(db.Model):
             "phone_number": self.phone_number,
             "email": self.email,
             "email_pref": self.email_pref,
+            "email_consent": self.email_consent,
             "text_pref": self.text_pref,
+            "text_consent": self.text_consent,
             "prob_pref": self.prob_pref,
+            "email_verification_sent": self.email_verification_sent,
+            "text_verification_sent": self.text_verification_sent,
         }
 
     def __repr__(self):
