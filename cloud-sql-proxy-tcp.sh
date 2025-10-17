aae0168f8c365f957eaa8d59d6f8dad6e014717f
mshukun_ncstate <mshukuno@users.noreply.github.com>
2025-10-17 11:10:38 -0400

    scripts: add run-web.sh for per-state web run; remove old cloud-sql-proxy scripts

--- cloud-sql-proxy-tcp.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-# TCP connection - shellcast-analysis
-./cloud-sql-proxy --port 3306 "{instance_connection_name}"
--- cloud-sql-proxy-unix.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Unix socket connection - shellcast-web-{state}
-instance_connection_name="{Find the instance connection name in the Google Cloud Console}"
-
-# Modify {state} to the state of the web application.
-./cloud-sql-proxy --unix-socket "./web/shellcast-web-{state}/cloudsql" ${instance_connection_name}
